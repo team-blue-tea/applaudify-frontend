b36b4050e2e591365167162c76355a9c3a805ebb
lvan-ni <gnilvan@users.noreply.github.com>
2023-10-23 13:57:13 +0200

    landing page basic styling done
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/assets/Logo.png';
+import title from '@/assets/title.png';
 
 const Landing = () => {
   return (
-    <>
+    <div className='landingPage'>
       <header>
-        {/* LOGO here */}
+        <Image
+        src={logo}
+        alt='logo'
+        width={46}
+        height={24}
+        ></Image>
         <Link href='/about'>About</Link>
         <Link href='https://github.com/orgs/team-blue-tea/repositories'>
           Docs
@@ -14,54 +22,61 @@ const Landing = () => {
       </header>
       <main>
         <section className='hero'>
-          <h1>applaudify</h1>
+          <Image
+          src={title}
+          alt='applaudify'
+          ></Image>
           <h3>Where achievements get applauded</h3>
           <p>
             Collect, manage, and showcase your professional testimonials -
             because every applaud matters.
           </p>
           <Link href='/login'>
-            <button>Let&apos;s Start</button>
+            <button className='startBtn'>Let&apos;s Start</button>
           </Link>
         </section>
-        <section>
-          <article>
-            <p>all your applauds in one place</p>
-            <h2>applauds inbox</h2>
+        <section className='features'>
+          <article className='singleFeature'>
+            <div className='header'>
+              <p>all your applauds in one place</p>
+              <h2>applauds inbox</h2>
+            </div>
             <p>
               A dedicated space to receive and manage all your accolades. Choose
               which applauds highlight your profile. Remember to acknowledge the
               remarkable work of others with an applause too!
             </p>
           </article>
-          <article>
-            <p>celebrate achievements together</p>
-            <h2>social feed</h2>
+          <article className='singleFeature'>
+            <div className='header'>
+              <p>celebrate achievements together</p>
+              <h2>social feed</h2>
+            </div>
             <p>
               Engage with the community. Revel in shared successes and
               accolades. Don&apos;t forget to showcase your achievements!
             </p>
           </article>
-          <article>
-            <p>redefining your professional profile</p>
-            <h2>dynamic profile</h2>
+          <article className='singleFeature'>
+            <div className='header'>
+              <p>redefining your professional profile</p>
+              <h2>dynamic profile</h2>
+            </div>
             <p>
               Beyond a typical CV—showcase your journey, skills, and
               experiences, validated by the applause you&apos;ve earned.
             </p>
           </article>
         </section>
-        <section>
-          <h3>ready to showcase your achievements?</h3>
-          <Link href='/login'>
-            <button>Let&apos;s Start</button>
-          </Link>
-        </section>
       </main>
       <footer>
+        <h3>ready to showcase your achievements?</h3>
+        <Link href='/login'>
+          <button className='startBtn'>Let&apos;s Start</button>
+        </Link>
         <p>© 2023 Applaudify</p>
       </footer>
-    </>
+    </div>
   );
 };
 
