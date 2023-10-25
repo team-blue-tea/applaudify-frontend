def16e3092b37a287565b10a407a53d5a0d50898
lvan-ni <g.nilvan@icloud.com>
2023-10-25 11:45:58 +0200

    feat: Compose page setup
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/compose/page.tsx
+++ src/app/compose/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import Link from 'next/link';
+// import InputEmoji from "react-input-emoji";
 
 const Compose = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log('applaud sent');
+  }
   return (
-    <div>compose</div>
-  )
-}
+    <div className='flex flex-col mx-10 mt-14 gap-10'>
+      <header className='flex justify-between'>
+        <Link href='/applauds'>←</Link>
+        <button type='submit' form='sendApplaud' onClick={handleSubmit}>Send</button>
+      </header>
+      <form id='sendApplaud' className='flex flex-col gap-10' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='To'
+        />
+        <input
+          type='text'
+          placeholder='Message'
+        />
+      </form>
 
-export default Compose
+    </div>
+  );
+};
+
+export default Compose;
