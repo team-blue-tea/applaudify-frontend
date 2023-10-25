eb4ca923565fd5e460e02e90020ff64070c6cfa7
lvan-ni <g.nilvan@icloud.com>
2023-10-25 11:03:18 +0200

    feat: adjust layout of the receiver card
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -18,7 +18,7 @@ const Home = () => {
       </header>
       <main>
         <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
-          <article className='flex gap-10 p-2 border-solid border border-stone'>
+          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
             <Image
               src={mockPhotoTwo}
               alt='Sender Profile'
@@ -29,18 +29,18 @@ const Home = () => {
               <p className='text-xs'>Sender Position, Company</p>
             </div>
           </article>
-          <article className='flex gap-10 p-2 border-solid border border-stone'>
+          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
+            <div className='flex flex-col text-right'>
+              <h4 className='text-sm'>Receiver Name</h4>
+              <p className='text-xs'>Receiver Position, Company</p>
+            </div>
             <Image
               src={mockPhotoOne}
               alt='Receiver Profile'
               width={50}
             ></Image>
-            <div className='flex flex-col'>
-              <h4 className='text-sm'>Receiver Name</h4>
-              <p className='text-xs'>Receiver Position, Company</p>
-            </div>
           </article>
-          <p>
+          <p className='text-center'>
             &apos; Working with Ilija is always a pleasure. Her positive
             attitude is infectious, and it lifts the team&apos;s spirits,
             especially when facing challenges. &apos;
