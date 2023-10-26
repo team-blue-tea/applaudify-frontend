147c3c82827e8ea643a38417c6f77a4b238d5f85
lvan-ni <g.nilvan@icloud.com>
2023-10-26 15:44:20 +0200

    feat: member type added
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

---
+++ src/types/MemberT.ts
@@ -0,0 +1,11 @@
+export type MemberT = {
+  id?: string;
+  email?: string;
+  name?: string;
+  jobTitle?: string;
+  company?: string;
+  avatarUrl?: string;
+  bio?: string;
+  skills?: string;
+  experience?: string;
+};
