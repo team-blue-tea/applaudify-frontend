1d9f5116cc830a55e62c6ee671e085c0fec74b50
lvan-ni <g.nilvan@icloud.com>
2023-10-26 15:47:36 +0200

    feat: config next auth
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Josefin_Sans } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Josefin_Sans } from 'next/font/google';
+import NextAuthProviders from '@/utils/nextAuthProviders';
+import './globals.css';
 
-const josefinSans = Josefin_Sans({ subsets: ['latin'] })
+const josefinSans = Josefin_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Applaudify',
   description: 'Where Achievements Get Applauded',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={josefinSans.className}>{children}</body>
+    <html lang='en'>
+      <body className={josefinSans.className}>
+        <NextAuthProviders>{children}</NextAuthProviders>
+      </body>
     </html>
-  )
+  );
 }
