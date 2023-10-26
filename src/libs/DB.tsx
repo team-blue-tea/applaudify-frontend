18c65c2d1560a08add7a0393306f2a5b16b45bda
lvan-ni <g.nilvan@icloud.com>
2023-10-26 15:46:39 +0200

    data: establish connection to the java backend
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

---
+++ src/libs/DB.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+const java_backend_uri = 'http://localhost:8080';
+
+const getAllApplauds = async () => {
+  try {
+    const uri = `${java_backend_uri}/api/v1/applauds`;
+    const res = await axios.get(uri);
+    return res.data;
+
+  } catch (error) {
+    console.error('An error occurred while fetching applauds -------->', error);
+    return null;
+  }
+};
+
+const getAllMembers = async () => {
+  try {
+    const uri = `${java_backend_uri}/api/v1/members`;
+    const res = await axios.get(uri);
+    return res.data;
+
+  } catch (error) {
+    console.error('An error occurred while fetching members -------->', error);
+    return null;
+  }
+}
+
+export { getAllApplauds, getAllMembers };
+
+// export async function getAllApplauds() {
+//   const response = await fetch(`${BACKEND_URL}/api/v1/applauds`);
+//   const applauds = await response.json();
+//   return applauds;
+// }
+
+// export async function getAllUsers() {
+//   const response = await fetch(`${BACKEND_URL}/api/v1/members`);
+//   const members = await response.json();
+//   return members;
+// }
+
+
+
+
+// export async function addUserToDb() {
+//     try {
+//         const
+//     }
+// }
