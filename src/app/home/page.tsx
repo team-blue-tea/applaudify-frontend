d6e4661aeec47869ad25a2c1522872ee33978769
lvan-ni <g.nilvan@icloud.com>
2023-10-27 16:17:55 +0200

    feat: creating applaud card
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import Ilija from '@/assets/Ilija.png';
-import Lvan from '@/assets/Lvan.png';
 import { getAllApplauds, getAllMembers } from '@/libs/DB';
-// import Menu from '@/components/Menu/Menu';
+import ApplaudCard from '@/components/ApplaudCard/ApplaudCard';
 
 const Home = async () => {
   const applauds = await getAllApplauds();
-  const members = await getAllMembers();
-  // console.log('Here come the applauds', applauds);
-  // console.log('Here come the members', members);
+  console.log('Here come the applauds', applauds);
 
   return (
     <div className='flex flex-col mx-10 mt-14 gap-10'>
@@ -26,36 +21,8 @@ const Home = async () => {
           </button>
         </Link>
       </header>
-      <main>
-        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
-          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
-            <Image
-              src={Lvan}
-              alt='Sender Profile'
-              width={50}
-            ></Image>
-            <div className='flex flex-col'>
-              <h4 className='text-sm'>Sender Name</h4>
-              <p className='text-xs'>Sender Position, Company</p>
-            </div>
-          </article>
-          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
-            <div className='flex flex-col text-right'>
-              <h4 className='text-sm'>Receiver Name</h4>
-              <p className='text-xs'>Receiver Position, Company</p>
-            </div>
-            <Image
-              src={Ilija}
-              alt='Receiver Profile'
-              width={50}
-            ></Image>
-          </article>
-          <p className='text-center'>
-            &apos; Working with Ilija is always a pleasure. Her positive
-            attitude is infectious, and it lifts the team&apos;s spirits,
-            especially when facing challenges. &apos;
-          </p>
-        </section>
+      <main className='flex flex-col gap-8 mt-6'>
+        <ApplaudCard applauds={applauds} />
       </main>
     </div>
   );
