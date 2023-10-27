667b1c86042c398ad81f95e11c5eb4239cac13fa
lvan-ni <g.nilvan@icloud.com>
2023-10-27 16:12:13 +0200

    feat: Applaud type added
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

---
+++ src/types/ApplaudT.ts
@@ -0,0 +1,22 @@
+export type ApplaudT = {
+  id: string,
+  sender: SenderT,
+  receiver: ReceiverT,
+  comment: string,
+}
+
+export type SenderT = {
+  id: string,
+  name: string,
+  jobTitle: string,
+  company?: string,
+  avatarUrl: string,
+}
+
+export type ReceiverT = {
+  id: string,
+  name: string,
+  jobTitle: string,
+  company?: string,
+  avatarUrl: string,
+}
