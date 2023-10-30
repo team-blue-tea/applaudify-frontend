543e5828908a18199f766b874ba8e77b219875b3
lvan-ni <g.nilvan@icloud.com>
2023-10-30 16:24:27 +0100

    feat: adding logic for sending a new applaud
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/compose/page.tsx
+++ src/app/compose/page.tsx
@@ -1,28 +1,49 @@
 'use client';
-import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import React, {
+  useEffect,
+  useState,
+  ChangeEvent,
+  FormEvent,
+  useRef,
+} from 'react';
 import Link from 'next/link';
-import { getAllMembers } from '@/libs/DB';
+import { getAllMembers, sendNewApplaud } from '@/libs/DB';
 import { MemberT } from '@/types/MemberT';
+import { useSession } from 'next-auth/react';
+import { NewApplaudT } from '@/types/NewApplaudT';
 
 const Compose = () => {
   const [members, setMembers] = useState<MemberT[]>([]);
   const [searchValue, setSearchValue] = useState<string>('');
   const [filteredMembers, setFilteredMembers] = useState<MemberT[]>([]);
+  const { data: session } = useSession();
+
+  const commentRef = useRef<HTMLTextAreaElement>(null);
+
+  const currentEmail = session?.user?.email;
+  const currentMember = members.find(
+    (member: MemberT) => member.email === currentEmail
+  );
+  const currentMemberId = { id: currentMember?.id };
+  const receiver = members.find(
+    (member: MemberT) => member.name === searchValue
+  );
+  const receiverId = { id: receiver?.id };
 
   useEffect(() => {
-    getAllMembers().then((members) => {
-      setMembers(members);
-    })
-    .catch((error) => {
-      console.error('Error fetching members:', error);
-    });
+    getAllMembers()
+      .then((members) => {
+        setMembers(members);
+      })
+      .catch((error) => {
+        console.error('Error fetching members:', error);
+      });
   }, []);
 
   useEffect(() => {
     setFilteredMembers(
-      members.filter(
-        (member) =>
-          member.name.toLowerCase().includes(searchValue.toLowerCase())
+      members.filter((member) =>
+        member.name.toLowerCase().includes(searchValue.toLowerCase())
       )
     );
   }, [searchValue, members]);
@@ -37,6 +58,16 @@ const Compose = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+
+    const applaudComment = commentRef.current?.value || '';
+
+    const newApplaud: NewApplaudT = {
+      sender: currentMemberId,
+      receiver: receiverId,
+      comment: applaudComment,
+    };
+    console.log(newApplaud);
+    sendNewApplaud(newApplaud);
   };
 
   return (
@@ -75,9 +106,10 @@ const Compose = () => {
               ))}
             </div>
           )}
-        <input
-          type='text'
-          placeholder='Message'
+        <textarea
+          rows={10}
+          placeholder='Comment'
+          ref={commentRef}
         />
       </form>
     </div>
