f01df69bc4a334ded2fd239092be41e44133cba5
lvan-ni <g.nilvan@icloud.com>
2023-10-30 16:23:09 +0100

    feat: reafctor login page
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,23 @@
-'use client'
-
+'use client';
 import React from 'react';
 import { signIn } from 'next-auth/react';
 
 const Login = () => {
   return (
-  <div className='h-screen flex flex-col items-center justify-center gap-10'>
-    <button className='w-1/2 h-10 border border-charcoal' onClick= {() => signIn('github', { callbackUrl: '/home'})}>Continue with GitHub</button>
-    <button className='w-1/2 h-10 border border-charcoal' >Continue with Google</button>
-    <button className='w-1/2 h-10 border border-charcoal' >Continue with LinkedIn</button>
-  </div>
+    <div className='h-screen flex flex-col items-center justify-center gap-10'>
+      <button
+        className='w-1/2 h-10 border border-charcoal'
+        onClick={() => signIn('github', { callbackUrl: '/home' })}
+      >
+        Continue with GitHub
+      </button>
+      <button className='w-1/2 h-10 border border-charcoal'>
+        Continue with Google
+      </button>
+      <button className='w-1/2 h-10 border border-charcoal'>
+        Continue with LinkedIn
+      </button>
+    </div>
   );
 };
 
