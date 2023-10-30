e28156b50f87f6ac34dddb035cb928d1e424f7de
lvan-ni <g.nilvan@icloud.com>
2023-10-30 16:21:05 +0100

    feat: comma changed to semi colon
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/types/ApplaudT.ts
+++ src/types/ApplaudT.ts
@@ -1,22 +1,22 @@
 export type ApplaudT = {
-  id: string,
-  sender: SenderT,
-  receiver: ReceiverT,
-  comment: string,
-}
+  id: string;
+  sender: SenderT;
+  receiver: ReceiverT;
+  comment: string;
+};
 
 export type SenderT = {
-  id: string,
-  name: string,
-  jobTitle: string,
-  company?: string,
-  avatarUrl: string,
-}
+  id: string;
+  name: string;
+  jobTitle: string;
+  company?: string;
+  avatarUrl: string;
+};
 
 export type ReceiverT = {
-  id: string,
-  name: string,
-  jobTitle: string,
-  company?: string,
-  avatarUrl: string,
-}
+  id: string;
+  name: string;
+  jobTitle: string;
+  company?: string;
+  avatarUrl: string;
+};
