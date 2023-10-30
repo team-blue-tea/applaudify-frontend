a02bb946f157691c6ad5c47029f4afac4748e7f3
lvan-ni <g.nilvan@icloud.com>
2023-10-30 16:27:52 +0100

    feat: adding mock data to the profile page
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,9 +7,13 @@ import Image from 'next/image';
 import Bio from '@/components/Bio/Bio';
 import Experience from '@/components/Experience/Experience';
 import Skills from '@/components/Skills/Skills';
+import { useSession } from 'next-auth/react';
 
 const Profile = () => {
   const [profileInfo, setProfileInfo] = useState(Bio);
+  const [profileApplauds, getProfileApplauds] = useState([]);
+  const { data: session } = useSession();
+  
 
   return (
     <div className='flex flex-col mx-10 mt-14 gap-10'>
@@ -41,27 +45,60 @@ const Profile = () => {
               src={Lvan}
               alt='Sender Profile'
               width={50}
+              height={50}
             ></Image>
             <div className='flex flex-col'>
-              <h4 className='text-sm'>Sender Name</h4>
-              <p className='text-xs'>Sender Position, Company</p>
+              <h4 className='text-sm'>Lvan Ni</h4>
+              <p className='text-xs'>Frontend Developer, Applaudify Inc.</p>
             </div>
           </article>
           <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
             <div className='flex flex-col text-right'>
-              <h4 className='text-sm'>Receiver Name</h4>
-              <p className='text-xs'>Receiver Position, Company</p>
+              <h4 className='text-sm'>{session?.user?.name}</h4>
+              <p className='text-xs'>Fullstack Developer, Freelance</p>
             </div>
             <Image
-              src={Ilija}
+              src={session?.user?.image!}
               alt='Receiver Profile'
               width={50}
+              height={50}
             ></Image>
           </article>
           <p className='text-center'>
-            &apos; Working with Ilija is always a pleasure. Her positive
-            attitude is infectious, and it lifts the team&apos;s spirits,
-            especially when facing challenges. &apos;
+            &apos; {session?.user?.name}&apos;s collaborative spirit and expertise in frontend 
+            development have contributed to the smooth interfacing of my backend work. A truly 
+            proficient developer who understands the full stack process. &apos;
+          </p>
+        </section>
+        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
+          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
+            <Image
+              src={Ilija}
+              alt='Sender Profile'
+              width={50}
+              height={50}
+            ></Image>
+            <div className='flex flex-col'>
+              <h4 className='text-sm'>Ilija Krilovic</h4>
+              <p className='text-xs'>Java Developer, TechCorp</p>
+            </div>
+          </article>
+          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
+            <div className='flex flex-col text-right'>
+            <h4 className='text-sm'>{session?.user?.name}</h4>
+            <p className='text-xs'>Fullstack Developer, Freelance</p>
+            </div>
+            <Image
+              src={session?.user?.image!}
+              alt='Receiver Profile'
+              width={50}
+              height={50}
+            ></Image>
+          </article>
+          <p className='text-center'>
+            &apos; {session?.user?.name}&apos;s commitment to excellence is evident in every line of code he writes. 
+            He consistently delivers high-quality work, often surpassing our expectations. 
+            It&apos;s always a pleasure working with him. &apos;
           </p>
         </section>
       </main>
