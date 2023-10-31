ac586fd84b80395bea398001828d6109130b255a
lvan-ni <g.nilvan@icloud.com>
2023-10-31 17:06:30 +0100

    refactor: refined profile page
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,54 +1,51 @@
-"use client";
-import React, { useState } from "react";
-import Link from "next/link";
-import Ilija from "@/assets/Ilija.png";
-import Lvan from "@/assets/Lvan.png";
-import Image from "next/image";
-import Bio from "@/components/Bio/Bio";
-import Experience from "@/components/Experience/Experience";
-import Skills from "@/components/Skills/Skills";
-import { useSession } from "next-auth/react";
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 const Profile = () => {
-  const [profileInfo, setProfileInfo] = useState(Bio);
-  const [profileApplauds, getProfileApplauds] = useState([]);
+  // const [profileInfo, setProfileInfo] = useState(Bio);
+  // const [profileApplauds, getProfileApplauds] = useState([]);
+  const [activeTab, setActiveTab] = useState('Bio');
   const { data: session } = useSession();
-  const [activeTab, setActiveTab] = useState("Bio");
+
+  const firstName = session?.user?.name?.split(' ')[0];
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
 
   return (
-    <div className="flex flex-col mx-10 mt-14 gap-10">
-      <header className="flex justify-between">
-        <Link href="/menu">
-          <button className="border-solid border border-charcoal px-4 py-1">
+    <div className='flex flex-col mx-10 mt-14 gap-10'>
+      <header className='flex justify-between'>
+        <Link href='/menu'>
+          <button className='border-solid border border-charcoal px-4 py-1'>
             Menu
           </button>
         </Link>
         <button>Edit</button>
       </header>
-      <main className="flex flex-col items-center gap-10">
-        <section className="flex flex-col gap-3 items-center w-full">
+      <main className='flex flex-col items-center gap-10'>
+        <section className='flex flex-col gap-3 items-center w-full'>
           <h4>{session?.user?.name}</h4>
           <Image
             src={session?.user?.image!}
-            alt="Profile photo"
+            alt='Profile photo'
             width={80}
             height={80}
-            className="rounded-full"
+            className='rounded-full'
           ></Image>
-          <article className="flex justify-around w-full">
-            <button onClick={() => handleTabClick("Bio")}>Bio</button>
-            <button onClick={() => handleTabClick("Skills")}>Skills</button>
-            <button onClick={() => handleTabClick("Experience")}>
+          <article className='flex justify-around w-full'>
+            <button onClick={() => handleTabClick('Bio')}>Bio</button>
+            <button onClick={() => handleTabClick('Skills')}>Skills</button>
+            <button onClick={() => handleTabClick('Experience')}>
               Experience
             </button>
           </article>
           <div>
-            {activeTab === "Bio" && (
-              <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+            {activeTab === 'Bio' && (
+              <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
                 <p>
                   Hey, I&apos;m {session?.user?.name}! With over 6 years of
                   experience as a frontend developer, I&apos;ve had the
@@ -59,10 +56,10 @@ const Profile = () => {
                 </p>
               </section>
             )}
-            {activeTab === "Skills" && (
-              <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+            {activeTab === 'Skills' && (
+              <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
                 <ul>
-                  <li>JavaScript (ES6+)</li>
+                  <li>JavaScript</li>
                   <li>React.js & Next.js</li>
                   <li>CSS, SCSS & TailwindCSS</li>
                   <li>Responsive Web Design</li>
@@ -73,8 +70,8 @@ const Profile = () => {
                 </ul>
               </section>
             )}
-            {activeTab === "Experience" && (
-              <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+            {activeTab === 'Experience' && (
+              <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
                 <ul>
                   <li>
                     Freelance Frontend Developer (2017-Present). Worked with
@@ -99,103 +96,111 @@ const Profile = () => {
             )}
           </div>
         </section>
-        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
-          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
+        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
+          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
             <Image
-              src={Lvan}
-              alt="Sender Profile"
+              src='https://avatars.githubusercontent.com/u/91157834?v=4'
+              alt='Sender Profile'
               width={50}
               height={50}
+              className='rounded-full'
             ></Image>
-            <div className="flex flex-col">
-              <h4 className="text-sm">Lvan Ni</h4>
-              <p className="text-xs">Frontend Developer, Applaudify Inc.</p>
+            <div className='flex flex-col'>
+              <h4 className='text-sm'>Hugo Dahlgren</h4>
+              <p className='text-xs'>Senior Software Engineer</p>
+              <p className='text-xs'>Tech Solutions Inc.</p>
             </div>
           </article>
-          <article className="flex justify-end items-center gap-6 p-2 border-solid border border-stone">
-            <div className="flex flex-col text-right">
-              <h4 className="text-sm">{session?.user?.name}</h4>
-              <p className="text-xs">Fullstack Developer, Freelance</p>
+          <article className='flex justify-end items-center gap-6 p-2 border-solid border border-stone'>
+            <div className='flex flex-col text-right'>
+              <h4 className='text-sm'>{session?.user?.name}</h4>
+              <p className='text-xs'>Fullstack Developer</p>
+              <p className='text-xs'>Freelance</p>
             </div>
             <Image
               src={session?.user?.image!}
-              alt="Receiver Profile"
+              alt='Receiver Profile'
               width={50}
               height={50}
-              className="rounded-full"
+              className='rounded-full'
             ></Image>
           </article>
-          <p className="text-center">
-            &apos;{session?.user?.name}&apos;s collaborative spirit and
-            expertise in frontend development have contributed to the smooth
-            interfacing of my backend work. A truly proficient developer who
-            understands the full stack process.&apos;
+          <p className='text-center'>
+            &apos;{firstName} is a great developer! I really enjoyed working
+            with {firstName} on this project. {firstName} is super easy to work
+            and making decisions with and teamwork together is a breeze. Looking
+            forward to work with you again in the future.&apos;
           </p>
         </section>
-        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
-          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
+        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
+          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
             <Image
-              src={Ilija}
-              alt="Sender Profile"
+              src='https://avatars.githubusercontent.com/u/101557392?v=4'
+              alt='Sender Profile'
               width={50}
               height={50}
+              className='rounded-full'
             ></Image>
-            <div className="flex flex-col">
-              <h4 className="text-sm">Ilija Krilovic</h4>
-              <p className="text-xs">Java Developer, TechCorp</p>
+            <div className='flex flex-col'>
+              <h4 className='text-sm'>Vanessa Wingårdh</h4>
+              <p className='text-xs'>Developer Team Lead</p>
+              <p className='text-xs'>DesignWeb Ltd.</p>
             </div>
           </article>
-          <article className="flex justify-end items-center gap-6 p-2 border-solid border border-stone">
-            <div className="flex flex-col text-right">
-              <h4 className="text-sm">{session?.user?.name}</h4>
-              <p className="text-xs">Fullstack Developer, Freelance</p>
+          <article className='flex justify-end items-center gap-6 p-2 border-solid border border-stone'>
+            <div className='flex flex-col text-right'>
+              <h4 className='text-sm'>{session?.user?.name}</h4>
+              <p className='text-xs'>Fullstack Developer</p>
+              <p className='text-xs'>Freelance</p>
             </div>
             <Image
               src={session?.user?.image!}
-              alt="Receiver Profile"
+              alt='Receiver Profile'
               width={50}
               height={50}
-              className="rounded-full"
+              className='rounded-full'
             ></Image>
           </article>
-          <p className="text-center">
-            &apos;{session?.user?.name} is a talented and dedicated software
-            developer, and I am grateful for their hard work and dedication. It
-            was a pleasure working with you.&apos;
+          <p className='text-center'>
+            &apos;{firstName} is a talented and dedicated software developer,
+            and I am grateful for their hard work and dedication. It was a
+            pleasure working with you.&apos;
           </p>
         </section>
-        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
-          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
+        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
+          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
             <Image
-              src={Ilija}
-              alt="Sender Profile"
+              src='https://avatars.githubusercontent.com/u/9844254?v=4'
+              alt='Sender Profile'
               width={50}
               height={50}
+              className='rounded-full'
             ></Image>
-            <div className="flex flex-col">
-              <h4 className="text-sm">Ilija Krilovic</h4>
-              <p className="text-xs">Java Developer, TechCorp</p>
+            <div className='flex flex-col'>
+              <h4 className='text-sm'>Muhammad Ahsan Ayaz</h4>
+              <p className='text-xs'>Principal Engineer</p>
+              <p className='text-xs'>Airbnb</p>
             </div>
           </article>
-          <article className="flex justify-end items-center gap-6 p-2 border-solid border border-stone">
-            <div className="flex flex-col text-right">
-              <h4 className="text-sm">{session?.user?.name}</h4>
-              <p className="text-xs">Fullstack Developer, Freelance</p>
+          <article className='flex justify-end items-center gap-6 p-2 border-solid border border-stone'>
+            <div className='flex flex-col text-right'>
+              <h4 className='text-sm'>{session?.user?.name}</h4>
+              <p className='text-xs'>Fullstack Developer</p>
+              <p className='text-xs'>Freelance</p>
             </div>
             <Image
               src={session?.user?.image!}
-              alt="Receiver Profile"
+              alt='Receiver Profile'
               width={50}
               height={50}
-              className="rounded-full"
+              className='rounded-full'
             ></Image>
           </article>
-          <p className="text-center">
-            &apos;Working along side {session?.user?.name} on various projects
-            has been nothing short of delightful.
-            {session?.user?.name} has a keen eye for detail and profound
-            understanding of user experience, which had an immense impact on the
-            outcome of the project.&apos;
+          <p className='text-center'>
+            &apos;Working along side {firstName} on various projects has been
+            nothing short of delightful. {firstName} has a keen eye for detail
+            and profound understanding of user experience, which had an immense
+            impact on the outcome of the project.&apos;
           </p>
         </section>
       </main>
