1e99ce550862f9a7281c80c3f2d8b41985adb180
lvan-ni <g.nilvan@icloud.com>
2023-11-01 10:54:23 +0100

    re-strucutre folders

--- src/app/components/ApplaudCard/ApplaudCard.tsx
+++ src/app/components/ApplaudCard/ApplaudCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { ApplaudT } from '@/types/ApplaudT';
+import { ApplaudT } from '@/app/types/ApplaudT';
 
 type ApplaudProps = {
   applauds: ApplaudT[];
