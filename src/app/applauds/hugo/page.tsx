622b917b9a987dcc80b37a903973014cc5ff45cf
ica1130 <ilijakrilovic@gmail.com>
2023-11-02 11:34:08 +0100

    refactor: Hugo applaud page.
    Implemented design from Figma with basic functionality.
    
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- src/app/applauds/hugo/page.tsx
+++ src/app/applauds/hugo/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
 
-const hugo = () => {
+const Hugo = () => {
+  const { data: session } = useSession();
+
+  const firstName = session?.user?.name?.split(" ")[0];
   return (
-    <div>hugo</div>
-  )
-}
+    <div className="flex flex-col mx-10 mt-14 gap-10">
+      <header className="flex justify-between">
+        <Link href="/applauds">←</Link>
+        <h4>Applaud from Hugo</h4>
+        <div>...</div>
+      </header>
+      <main className="flex flex-col items-center gap-10">
+        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
+            <Image
+              src="https://avatars.githubusercontent.com/u/91157834?v=4"
+              alt="Sender Profile"
+              width={50}
+              height={50}
+              className="rounded-full"
+            ></Image>
+            <div className="flex flex-col">
+              <h4 className="text-sm">Hugo Dahlgren</h4>
+              <p className="text-xs">Senior Software Engineer</p>
+              <p className="text-xs">Tech Solutions Inc.</p>
+            </div>
+          </article>
+          <p className="text-center">
+            &apos;{firstName} is a great developer! I really enjoyed working
+            with {firstName} on this project. {firstName} is super easy to work
+            with and making decisions together is a breeze. Looking forward to
+            work with you again in the future.&apos;
+          </p>
+        </section>
+        <p className="text-center border-solid border border-metal p-2">
+          Applaud published.
+        </p>
+      </main>
+    </div>
+  );
+};
 
-export default hugo
+export default Hugo;
