0e798117514a46800909f4ef8a5e19223ccb65ad
lvan-ni <g.nilvan@icloud.com>
2023-11-06 16:03:05 +0100

    Update GitHubProvider credentials in options.ts
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,8 +4,8 @@ import GitHubProvider from 'next-auth/providers/github';
 export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID_PROD as string || process.env.GITHUB_ID_DEV as string,
+      clientSecret: process.env.GITHUB_SECRET_PROD as string || process.env.GITHUB_SECRET_DEV as string,
     }),
   ],
 };
