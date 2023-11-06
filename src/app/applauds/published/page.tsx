68e08ca97694d0836fe800cf9dca57f2d2745eee
lvan-ni <g.nilvan@icloud.com>
2023-11-06 11:14:15 +0100

    Commented out useEffect and modified return statement in Published component
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/applauds/published/page.tsx
+++ src/app/applauds/published/page.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react'
+// 'use client';
+// import React, { useEffect } from 'react';
 
-const Published = () => {
-  useEffect(() => {
-    (() => {
-      setTimeout(() => {
-        window.location.href = '/applauds';
-      }, 2000);
-    })();
-  }, []);
+// const Published = () => {
+//   useEffect(() => {
+//     (() => {
+//       setTimeout(() => {
+//         window.history.back();
+//       }, 1000);
+//     })();
+//   }, []);
 
-  return (
-    <main className='flex justify-center items-center w-screen h-screen'>
-      <h2>Applaud published!</h2>
-    </main>
-  );
-};
+//   return (
+//     <main className='flex justify-center items-center w-screen h-screen'>
+//       <h2>Applaud published!</h2>
+//     </main>
+//   );
+// };
 
-export default Published
+// export default Published;
