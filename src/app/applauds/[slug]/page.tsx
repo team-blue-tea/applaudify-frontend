da718c81c5fb21b81135258b7bda44acabea5839
ica1130 <ilijakrilovic@gmail.com>
2023-11-06 16:50:57 +0100

    refactor Components.

--- src/app/applauds/[slug]/page.tsx
+++ src/app/applauds/[slug]/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { getAllApplauds } from '@/libs/DB';
-import { ApplaudT } from '@/types/ApplaudT';
-import PublishButton from '@/components/PublishButton/PublishButton';
-import UnpublishButton from '@/components/UnpublishButton/page';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { getAllApplauds } from "@/libs/DB";
+import { ApplaudT } from "@/types/ApplaudT";
+import PublishButton from "@/Components/PublishButton/PublishButton";
+import UnpublishButton from "@/Components/UnpublishButton/page";
 
 const SingleApplaud = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
@@ -14,33 +14,33 @@ const SingleApplaud = async ({ params }: { params: { slug: string } }) => {
   const { sender, comment, published } = filteredApplaud[0];
 
   return (
-    <div className='flex flex-col mx-10 mt-14 gap-10'>
-      <header className='flex justify-between'>
-        <Link href='/applauds'>←</Link>
-        <h4>Applaud from {sender.name.split(' ')[0]}</h4>
+    <div className="flex flex-col mx-10 mt-14 gap-10">
+      <header className="flex justify-between">
+        <Link href="/applauds">←</Link>
+        <h4>Applaud from {sender.name.split(" ")[0]}</h4>
         <div></div>
       </header>
-      <main className='flex flex-col items-center gap-10'>
-        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
-          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
+      <main className="flex flex-col items-center gap-10">
+        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
             <Image
               src={sender.avatarUrl}
-              alt='Sender Profile'
+              alt="Sender Profile"
               width={50}
               height={50}
-              className='rounded-full'
+              className="rounded-full"
             ></Image>
-            <div className='flex flex-col'>
-              <h4 className='text-sm'>{sender.name}</h4>
-              <p className='text-xs'>{sender.jobTitle}</p>
-              <p className='text-xs'>{sender.company}</p>
+            <div className="flex flex-col">
+              <h4 className="text-sm">{sender.name}</h4>
+              <p className="text-xs">{sender.jobTitle}</p>
+              <p className="text-xs">{sender.company}</p>
             </div>
           </article>
-          <p className='text-center'>{comment}</p>
+          <p className="text-center">{comment}</p>
         </section>
         {published ? (
-          <div className='flex flex-col items-center gap-10'>
-            <p className='text-center'>Applaud Published!</p>
+          <div className="flex flex-col items-center gap-10">
+            <p className="text-center">Applaud Published!</p>
             <UnpublishButton slug={slug} />
           </div>
         ) : (
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,20 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { useSession } from 'next-auth/react';
-import ApplaudCard from '@/components/ApplaudCard/ApplaudCard';
-import { getPublishedApplaudsByMemberEmail } from '@/libs/DB';
-import { ApplaudT } from '@/types/ApplaudT';
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import ApplaudCard from "@/Components/ApplaudCard/ApplaudCard";
+import { getPublishedApplaudsByMemberEmail } from "@/libs/DB";
+import { ApplaudT } from "@/types/ApplaudT";
 
 const Profile = () => {
   // const [profileInfo, setProfileInfo] = useState(Bio);
   // const [profileApplauds, getProfileApplauds] = useState([]);
-  const [activeTab, setActiveTab] = useState('Bio');
+  const [activeTab, setActiveTab] = useState("Bio");
   const [publishedApplauds, setPublishedApplauds] = useState<ApplaudT[]>([]);
   const { data: session } = useSession();
 
-  const firstName = session?.user?.name?.split(' ')[0];
+  const firstName = session?.user?.name?.split(" ")[0];
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
@@ -33,35 +33,35 @@ const Profile = () => {
   }, [session]);
 
   return (
-    <div className='flex flex-col mx-10 mt-14 gap-10'>
-      <header className='flex justify-between'>
-        <Link href='/menu'>
-          <button className='border-solid border border-charcoal px-4 py-1'>
+    <div className="flex flex-col mx-10 mt-14 gap-10">
+      <header className="flex justify-between">
+        <Link href="/menu">
+          <button className="border-solid border border-charcoal px-4 py-1">
             Menu
           </button>
         </Link>
         <button>Edit</button>
       </header>
-      <main className='flex flex-col items-center gap-10'>
-        <section className='flex flex-col gap-3 items-center w-full'>
+      <main className="flex flex-col items-center gap-10">
+        <section className="flex flex-col gap-3 items-center w-full">
           <h4>{session?.user?.name}</h4>
           <Image
             src={session?.user?.image!}
-            alt='Profile photo'
+            alt="Profile photo"
             width={80}
             height={80}
-            className='rounded-full'
+            className="rounded-full"
           ></Image>
-          <article className='flex justify-around w-full'>
-            <button onClick={() => handleTabClick('Bio')}>Bio</button>
-            <button onClick={() => handleTabClick('Skills')}>Skills</button>
-            <button onClick={() => handleTabClick('Experience')}>
+          <article className="flex justify-around w-full">
+            <button onClick={() => handleTabClick("Bio")}>Bio</button>
+            <button onClick={() => handleTabClick("Skills")}>Skills</button>
+            <button onClick={() => handleTabClick("Experience")}>
               Experience
             </button>
           </article>
           <div>
-            {activeTab === 'Bio' && (
-              <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
+            {activeTab === "Bio" && (
+              <section className="flex flex-col gap-4 border-solid border border-metal p-4">
                 <p>
                   Hey, I&apos;m {session?.user?.name}! With over 6 years of
                   experience as a frontend developer, I&apos;ve had the
@@ -72,8 +72,8 @@ const Profile = () => {
                 </p>
               </section>
             )}
-            {activeTab === 'Skills' && (
-              <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
+            {activeTab === "Skills" && (
+              <section className="flex flex-col gap-4 border-solid border border-metal p-4">
                 <ul>
                   <li>JavaScript</li>
                   <li>React.js & Next.js</li>
@@ -86,8 +86,8 @@ const Profile = () => {
                 </ul>
               </section>
             )}
-            {activeTab === 'Experience' && (
-              <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
+            {activeTab === "Experience" && (
+              <section className="flex flex-col gap-4 border-solid border border-metal p-4">
                 <ul>
                   <li>
                     Freelance Frontend Developer (2017-Present). Worked with
@@ -115,107 +115,107 @@ const Profile = () => {
         <section>
           <ApplaudCard applauds={publishedApplauds} />
         </section>
-        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
-          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
+        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
             <Image
-              src='https://avatars.githubusercontent.com/u/91157834?v=4'
-              alt='Sender Profile'
+              src="https://avatars.githubusercontent.com/u/91157834?v=4"
+              alt="Sender Profile"
               width={50}
               height={50}
-              className='rounded-full'
+              className="rounded-full"
             ></Image>
-            <div className='flex flex-col'>
-              <h4 className='text-sm'>Hugo Dahlgren</h4>
-              <p className='text-xs'>Senior Software Engineer</p>
-              <p className='text-xs'>Tech Solutions Inc.</p>
+            <div className="flex flex-col">
+              <h4 className="text-sm">Hugo Dahlgren</h4>
+              <p className="text-xs">Senior Software Engineer</p>
+              <p className="text-xs">Tech Solutions Inc.</p>
             </div>
           </article>
-          <article className='flex justify-end items-center gap-6 p-2 border-solid border border-stone'>
-            <div className='flex flex-col text-right'>
-              <h4 className='text-sm'>{session?.user?.name}</h4>
-              <p className='text-xs'>Fullstack Developer</p>
-              <p className='text-xs'>Freelance</p>
+          <article className="flex justify-end items-center gap-6 p-2 border-solid border border-stone">
+            <div className="flex flex-col text-right">
+              <h4 className="text-sm">{session?.user?.name}</h4>
+              <p className="text-xs">Fullstack Developer</p>
+              <p className="text-xs">Freelance</p>
             </div>
             <Image
               src={session?.user?.image!}
-              alt='Receiver Profile'
+              alt="Receiver Profile"
               width={50}
               height={50}
-              className='rounded-full'
+              className="rounded-full"
             ></Image>
           </article>
-          <p className='text-center'>
+          <p className="text-center">
             &apos;{firstName} is a great developer! I really enjoyed working
             with {firstName} on this project. {firstName} is super easy to work
             with and making decisions together is a breeze. Looking forward to
             work with you again in the future.&apos;
           </p>
         </section>
-        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
-          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
+        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
             <Image
-              src='https://avatars.githubusercontent.com/u/101557392?v=4'
-              alt='Sender Profile'
+              src="https://avatars.githubusercontent.com/u/101557392?v=4"
+              alt="Sender Profile"
               width={50}
               height={50}
-              className='rounded-full'
+              className="rounded-full"
             ></Image>
-            <div className='flex flex-col'>
-              <h4 className='text-sm'>Vanessa Wingårdh</h4>
-              <p className='text-xs'>Developer Team Lead</p>
-              <p className='text-xs'>DesignWeb Ltd.</p>
+            <div className="flex flex-col">
+              <h4 className="text-sm">Vanessa Wingårdh</h4>
+              <p className="text-xs">Developer Team Lead</p>
+              <p className="text-xs">DesignWeb Ltd.</p>
             </div>
           </article>
-          <article className='flex justify-end items-center gap-6 p-2 border-solid border border-stone'>
-            <div className='flex flex-col text-right'>
-              <h4 className='text-sm'>{session?.user?.name}</h4>
-              <p className='text-xs'>Fullstack Developer</p>
-              <p className='text-xs'>Freelance</p>
+          <article className="flex justify-end items-center gap-6 p-2 border-solid border border-stone">
+            <div className="flex flex-col text-right">
+              <h4 className="text-sm">{session?.user?.name}</h4>
+              <p className="text-xs">Fullstack Developer</p>
+              <p className="text-xs">Freelance</p>
             </div>
             <Image
               src={session?.user?.image!}
-              alt='Receiver Profile'
+              alt="Receiver Profile"
               width={50}
               height={50}
-              className='rounded-full'
+              className="rounded-full"
             ></Image>
           </article>
-          <p className='text-center'>
+          <p className="text-center">
             &apos;{firstName} is a talented and dedicated software developer,
             and I am grateful for their hard work and dedication. It was a
             pleasure working with you.&apos;
           </p>
         </section>
-        <section className='flex flex-col gap-4 border-solid border border-metal p-4'>
-          <article className='flex items-center gap-6 p-2 border-solid border border-stone'>
+        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
+          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
             <Image
-              src='https://avatars.githubusercontent.com/u/9844254?v=4'
-              alt='Sender Profile'
+              src="https://avatars.githubusercontent.com/u/9844254?v=4"
+              alt="Sender Profile"
               width={50}
               height={50}
-              className='rounded-full'
+              className="rounded-full"
             ></Image>
-            <div className='flex flex-col'>
-              <h4 className='text-sm'>Muhammad Ahsan Ayaz</h4>
-              <p className='text-xs'>Principal Engineer</p>
-              <p className='text-xs'>Airbnb</p>
+            <div className="flex flex-col">
+              <h4 className="text-sm">Muhammad Ahsan Ayaz</h4>
+              <p className="text-xs">Principal Engineer</p>
+              <p className="text-xs">Airbnb</p>
             </div>
           </article>
-          <article className='flex justify-end items-center gap-6 p-2 border-solid border border-stone'>
-            <div className='flex flex-col text-right'>
-              <h4 className='text-sm'>{session?.user?.name}</h4>
-              <p className='text-xs'>Fullstack Developer</p>
-              <p className='text-xs'>Freelance</p>
+          <article className="flex justify-end items-center gap-6 p-2 border-solid border border-stone">
+            <div className="flex flex-col text-right">
+              <h4 className="text-sm">{session?.user?.name}</h4>
+              <p className="text-xs">Fullstack Developer</p>
+              <p className="text-xs">Freelance</p>
             </div>
             <Image
               src={session?.user?.image!}
-              alt='Receiver Profile'
+              alt="Receiver Profile"
               width={50}
               height={50}
-              className='rounded-full'
+              className="rounded-full"
             ></Image>
           </article>
-          <p className='text-center'>
+          <p className="text-center">
             &apos;Working along side {firstName} on various projects has been
             nothing short of delightful. {firstName} has a keen eye for detail
             and profound understanding of user experience, which had an immense
