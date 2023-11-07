2121f63eda1164cd3c12775d9113954d7eb6a04e
lvan-ni <g.nilvan@icloud.com>
2023-11-07 16:27:42 +0100

    Add sign out button to menu
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,13 +1,23 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/react';
 
 const Menu = () => {
+  const { data: session } = useSession();
   return (
     <div className='h-screen flex flex-col items-center justify-center gap-20 text-xl'>
       {/* <Link href='/search'>Search</Link> */}
       <Link href='/home'>Home</Link>
       <Link href='/profile'>Profile</Link>
       <Link href='/about'>About</Link>
+      {session && (
+        <button
+          onClick={() => signOut({ callbackUrl: '/' })}
+        >
+          Sign out
+        </button>
+      )}
     </div>
   );
 };
