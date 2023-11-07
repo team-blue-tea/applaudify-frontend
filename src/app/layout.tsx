1372bfd9b6d90abf7d744c6f93ddaf2edbd5f14e
lvan-ni <g.nilvan@icloud.com>
2023-11-07 13:19:29 +0100

    Add new fonts to layout.tsx
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,22 @@
 import type { Metadata } from 'next';
 import NextAuthProviders from '@/utils/nextAuthProviders';
-import { Josefin_Sans } from 'next/font/google';
+import { Josefin_Sans, Raleway } from 'next/font/google';
+import localFont from 'next/font/local'
 import './globals.css';
+// import gilgan from './../font/Gilgan/Gilgan.otf';
 
 const josefin = Josefin_Sans({ subsets: ['latin'] });
+const raleway = Raleway({ subsets: ['latin'] });
+
+const gilgan = localFont({
+  src: './../font/Gilgan/Gilgan.otf',
+  display: 'swap',
+});
+
+const tangoSans = localFont({
+  src: './../font/TangoSans/TangoSans.ttf',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Applaudify',
