7eab95dad6fdfac98b529d6f17a6f4344ddd58d4
lvan-ni <g.nilvan@icloud.com>
2023-11-07 09:54:11 +0100

    Add Josefin Sans font to body className
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
 import NextAuthProviders from '@/utils/nextAuthProviders';
+import { Josefin_Sans } from 'next/font/google';
 import './globals.css';
 
+const josefin = Josefin_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Applaudify',
@@ -15,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>
+      <body className={josefin.className}>
         <NextAuthProviders>{children}</NextAuthProviders>
       </body>
     </html>
