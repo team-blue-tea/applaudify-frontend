96bf28d7e3d2738747bd11ebd3d596ebffd63ad5
lvan-ni <g.nilvan@icloud.com>
2023-11-07 09:33:25 +0100

    Refactor code for improved readability and efficiency.

---
+++ src/components/PublishButton/PublishButton.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from 'react';
+import { setApplaudPublished } from '@/libs/DB';
+
+type PublishButtonProps = {
+  slug: string;
+};
+
+const PublishButton: React.FC<PublishButtonProps> = ({ slug }) => {
+  const handlePublishClick = () => {
+    setApplaudPublished(slug);
+    setTimeout(() => {
+      window.location.href = '/home';
+    }, 1000);
+
+  };
+
+  return (
+    <button
+      className='text-center border-solid border border-metal p-2'
+      onClick={handlePublishClick}
+    >
+      Publish to Profile
+    </button>
+  );
+};
+
+export default PublishButton;
