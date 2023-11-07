106beff31a9db7c99ff14302c4ad7ce991a0e3d4
ica1130 <ilijakrilovic@gmail.com>
2023-11-07 23:44:09 +0100

    refactor: Redirect after un/publish
    Updated code for redirect page after un/publish.
    
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- src/app/applauds/unpublished/page.tsx
+++ src/app/applauds/unpublished/page.tsx
@@ -1,17 +1,22 @@
-'use client';
-import React, { useEffect } from 'react';
+"use client";
+import React, { useEffect } from "react";
 
 const Unpublished = () => {
   useEffect(() => {
-    (() => {
-      setTimeout(() => {
-        window.history.back();
-      }, 1000);
-    })();
+    // Set a meta refresh tag to trigger a redirect after half a second
+    const metaRefresh = document.createElement("meta");
+    metaRefresh.setAttribute("http-equiv", "refresh");
+    metaRefresh.setAttribute("content", "0; url=" + document.referrer);
+    document.head.appendChild(metaRefresh);
+
+    // Remove the meta refresh tag after half a second to prevent infinite redirects
+    setTimeout(() => {
+      document.head.removeChild(metaRefresh);
+    }, 500);
   }, []);
 
   return (
-    <main className='flex justify-center items-center w-screen h-screen'>
+    <main className="flex justify-center items-center w-screen h-screen">
       <h2>Publish Reverted!</h2>
     </main>
   );
