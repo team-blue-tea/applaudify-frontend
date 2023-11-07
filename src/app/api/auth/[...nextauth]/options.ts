952812f20de5b0760e7139b50fea2be2885d63ed
lvan-ni <g.nilvan@icloud.com>
2023-11-07 13:19:34 +0100

    Add LinkedIn provider to NextAuth options.
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,16 @@
 import type { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import LinkedInProvider from "next-auth/providers/linkedin";
 
 export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID_PROD as string || process.env.GITHUB_ID_DEV as string,
       clientSecret: process.env.GITHUB_SECRET_PROD as string || process.env.GITHUB_SECRET_DEV as string,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID as string,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
     }),
   ],
 };
