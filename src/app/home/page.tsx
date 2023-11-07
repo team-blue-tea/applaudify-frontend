7ba7d05f26950639afe7a39230dde803aaf81aec
lvan-ni <g.nilvan@icloud.com>
2023-11-07 09:36:52 +0100

    debug name case sensitivity issue

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,22 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
+'use client';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import {
   getAllApplauds,
   getAllMembers,
   addNewMember,
   getNumberOfUnreadApplaudsByMemberEmail,
-} from "@/libs/DB";
-import { useSession } from "next-auth/react";
-import { ApplaudT } from "@/types/ApplaudT";
-import { MemberT } from "@/types/MemberT";
-import ApplaudCard from "@/Components/ApplaudCard/ApplaudCard";
+} from '@/libs/DB';
+import { useSession } from 'next-auth/react';
+import { ApplaudT } from '@/types/ApplaudT';
+import { MemberT } from '@/types/MemberT';
+import ApplaudCard from '@/things/ApplaudCard/ApplaudCard';
 
 const Home = () => {
   const [applauds, setApplauds] = useState<ApplaudT[]>([]);
   const [existingMembers, setExistingMembers] = useState<MemberT[]>([]);
   const { data: session } = useSession();
-  const [notifications, setNotifications] = useState<string>("");
+  const [notifications, setNotifications] = useState<string>('');
 
   useEffect(() => {
     if (!session) {
@@ -63,20 +63,20 @@ const Home = () => {
   }, [existingMembers, session]);
 
   return (
-    <div className="flex flex-col mx-10 mt-14 gap-10">
-      <header className="flex w-full justify-between">
-        <Link href="/menu">
-          <button className="border-solid border border-charcoal px-4 py-1">
+    <div className='flex flex-col mx-10 mt-14 gap-10'>
+      <header className='flex w-full justify-between'>
+        <Link href='/menu'>
+          <button className='border-solid border border-charcoal px-4 py-1'>
             Menu
           </button>
         </Link>
-        <Link href="/applauds">
-          <button className="border-solid border border-charcoal px-4 py-1">
+        <Link href='/applauds'>
+          <button className='border-solid border border-charcoal px-4 py-1'>
             {notifications} Applauds
           </button>
         </Link>
       </header>
-      <main className="flex flex-col gap-8 mt-6">
+      <main className='flex flex-col gap-8 mt-6'>
         <ApplaudCard applauds={applauds} />
       </main>
     </div>
