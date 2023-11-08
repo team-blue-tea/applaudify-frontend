af9835f2469a57a4eb18bafb8a3dd943239fb6f2
ica1130 <ilijakrilovic@gmail.com>
2023-11-08 10:23:19 +0100

    refactor: compose page.
    Created validation for imput for Member and Comment.
    
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- src/app/compose/page.tsx
+++ src/app/compose/page.tsx
@@ -1,24 +1,25 @@
-'use client';
+"use client";
 import React, {
   useEffect,
   useState,
   ChangeEvent,
   FormEvent,
   useRef,
-} from 'react';
-import Link from 'next/link';
-import { getAllMembers, sendNewApplaud } from '@/libs/DB';
-import { MemberT } from '@/types/MemberT';
-import { useSession } from 'next-auth/react';
-import { NewApplaudT } from '@/types/NewApplaudT';
+} from "react";
+import Link from "next/link";
+import { getAllMembers, sendNewApplaud } from "@/libs/DB";
+import { MemberT } from "@/types/MemberT";
+import { useSession } from "next-auth/react";
+import { NewApplaudT } from "@/types/NewApplaudT";
 
 const Compose = () => {
   const [members, setMembers] = useState<MemberT[]>([]);
-  const [toText, setToText] = useState<string>('To');
-  const [applaudText, setApplaudText] = useState<string>('Applaud');
-  const [searchValue, setSearchValue] = useState<string>('');
+  const [toText, setToText] = useState<string>("To");
+  const [applaudText, setApplaudText] = useState<string>("Applaud");
+  const [searchValue, setSearchValue] = useState<string>("");
   const [filteredMembers, setFilteredMembers] = useState<MemberT[]>([]);
   const { data: session } = useSession();
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const commentRef = useRef<HTMLTextAreaElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
@@ -39,7 +40,7 @@ const Compose = () => {
         setMembers(members);
       })
       .catch((error) => {
-        console.error('Error fetching members:', error);
+        console.error("Error fetching members:", error);
       });
   }, []);
 
@@ -57,13 +58,25 @@ const Compose = () => {
 
   const handleMemberSelect = (name: string) => {
     setSearchValue(name);
+    setErrorMessage("");
   };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    const applaudComment = commentRef.current?.value || '';
+    const applaudComment = commentRef.current?.value || "";
 
+    if (!members.some((member) => member.name === searchValue)) {
+      setErrorMessage("Please select the receiver");
+      return;
+    }
+    if (!applaudComment || applaudComment.trim() === "") {
+      setErrorMessage("Let's finish the Applaud before sending");
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 2000);
+      return;
+    }
     const newApplaud: NewApplaudT = {
       sender: currentMemberId,
       receiver: receiverId,
@@ -74,40 +87,47 @@ const Compose = () => {
     form?.reset();
 
     setTimeout(() => {
-      window.location.href = '/applauds/sent';
+      window.location.href = "/applauds/sent";
     }, 500);
   };
 
   const handleFocus = () => {
-    setToText('Type to search...');
-    setApplaudText('Start your applaud here...' );
-  }
+    setToText("Type to search...");
+    setApplaudText("Start your applaud here...");
+  };
 
   const handleBlur = () => {
-    setToText('To');
-    setApplaudText('Applaud');
-  }
+    setToText("To");
+    setApplaudText("Applaud");
+  };
 
   return (
-    <div className='flex flex-col mx-10 mt-14 gap-10'>
-      <header className='flex justify-between'>
-        <Link href='/applauds'>←</Link>
-        <button
-          type='submit'
-          form='sendApplaud'
-          onClick={handleSubmit}
-        >
-          Send
-        </button>
+    <div className="flex flex-col mx-10 mt-14 gap-10">
+      <header className="flex justify-between">
+        <Link href="/applauds">←</Link>
+        {searchValue && filteredMembers.length > 0 ? (
+          <button type="submit" form="sendApplaud" onClick={handleSubmit}>
+            Send
+          </button>
+        ) : (
+          <button
+            className="text-silver"
+            type="submit"
+            form="sendApplaud"
+            disabled
+          >
+            Send
+          </button>
+        )}
       </header>
       <form
-        id='sendApplaud'
-        className='flex flex-col gap-10'
+        id="sendApplaud"
+        className="flex flex-col gap-10"
         onSubmit={handleSubmit}
         ref={formRef}
       >
         <input
-          type='text'
+          type="text"
           placeholder={toText}
           value={searchValue}
           onChange={handleInputChange}
@@ -115,6 +135,7 @@ const Compose = () => {
           onBlur={handleBlur}
         />
         {searchValue &&
+          filteredMembers.length > 0 &&
           (filteredMembers[0].name as string) !== (searchValue as string) && (
             <div>
               {filteredMembers.map((member: MemberT) => (
@@ -127,6 +148,10 @@ const Compose = () => {
               ))}
             </div>
           )}
+        {searchValue && filteredMembers.length === 0 && (
+          <div>No Member found</div>
+        )}
+        {errorMessage}
         <textarea
           rows={10}
           placeholder={applaudText}
