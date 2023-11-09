2e2d16072574f660fdc72c320f0c836209a0600f
lvan-ni <g.nilvan@icloud.com>
2023-11-09 17:27:01 +0100

    Add logo and close menu button to menu page
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,23 +1,44 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/assets/logo.png';
+import closeMenu from '@/assets/nav/close-menu.png';
+import { useRouter } from 'next/navigation';
 import { useSession, signOut } from 'next-auth/react';
 
 const Menu = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+
   return (
-    <div className='h-screen flex flex-col items-center justify-center gap-20 text-xl'>
-      {/* <Link href='/search'>Search</Link> */}
-      <Link href='/home'>Home</Link>
-      <Link href='/profile'>Profile</Link>
-      <Link href='/about'>About</Link>
-      {session && (
-        <button
-          onClick={() => signOut({ callbackUrl: '/' })}
-        >
-          Sign out
-        </button>
-      )}
+    <div className='h-screen flex flex-col items-center justify-center gap-40 text-xl'>
+      <button onClick={() => router.back()}>
+        <Image
+          src={closeMenu}
+          alt='close menu'
+          width={30}
+          height={30}
+        ></Image>
+      </button>
+      <div className='flex flex-col items-center gap-20 h-1/2'>
+        <Link href='/'>
+          <Image
+            src={logo}
+            alt='logo'
+            width={30}
+            height={30}
+          ></Image>
+        </Link>
+        {session && (
+          <button
+            className='sub-title'
+            onClick={() => signOut({ callbackUrl: '/' })}
+          >
+            Sign out
+          </button>
+        )}
+      </div>
     </div>
   );
 };
