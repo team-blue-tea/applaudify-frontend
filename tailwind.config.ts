7e6f72eb1ed0d959efc20671f44618e83ee50350
lvan-ni <g.nilvan@icloud.com>
2023-11-09 13:05:56 +0100

    Update Tailwind config with new colors, font sizes, and background image
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,50 +2,42 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        black: '#000000',
-        metal: '#0d0d0d',
-        charcoal: '#303030',
-        stone: '#626262',
-        silver: '#bab8b5',
-        paper: '#f8f6f2',
-        white: '#ffffff',
-        cream: '#f1ebe2',
-        sunlight: '#fbe880',
-        lime: '#e4f285',
-        sakura: '#ffaec8',
-        sky: '#a7d0fb',
-      },
-      backgroundImage: {
-        ombre:
-          'linear-gradient(91deg, #FBE880 7.04%, #E4F285 35.37%, #FFAEC8 65.05%, #A7D0FB 93.38%)',
-      },
-      fontFamily: {
-        josefin: ['Josefin Sans', 'sans-serif'],
-        avenir: ['Avenir Next', 'sans-serif'],
-      },
-      fontSize: {
-        '10': '10px',
-        '12': '12px',
-        '14': '14px',
-        '16': '16px',
-        '18': '18px',
-        '30': '30px',
-      },
-      fontWeight: {
-        normal: '400',
-        medium: '500',
-        semibold: '600',
-        bold: '700',
-      },
+    colors: {
+      black: '#000000',
+      metal: '#0d0d0d',
+      charcoal: '#303030',
+      stone: '#626262',
+      silver: '#bab8b5',
+      paper: '#f8f6f2',
+      white: '#ffffff',
+
+    },
+    backgroundImage: {
+      logo: 'linear-gradient(125deg, #68D0F9 16.46%, #9ED7F3 36.98%, #D7BDF5 51.69%, #CC81F1 60.85%, #E96069 71.67%, #F09674 91.21%)',
+    },
+    fontFamily: {
+      josefin: ['Josefin Sans', 'sans-serif'],
+      avenir: ['Avenir Next', 'sans-serif'],
+    },
+    fontSize: {
+      '10': '10px',
+      '12': '12px',
+      '14': '14px',
+      '16': '16px',
+      '18': '18px',
+      '30': '30px',
+    },
+    fontWeight: {
+      400: '400',
+      500: '500',
+      600: '600',
+      700: '700',
     },
   },
   plugins: [],
 };
+
 export default config;
