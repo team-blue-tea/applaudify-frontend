59d63ad608ab6f0377b602ba622c803eaabf003c
lvan-ni <g.nilvan@icloud.com>
2023-11-09 17:25:59 +0100

    Add new colors, background image, and font sizes to theme.
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    extend: {
     colors: {
       black: '#000000',
       metal: '#0d0d0d',
@@ -13,10 +14,9 @@ const config: Config = {
       silver: '#bab8b5',
       paper: '#f8f6f2',
       white: '#ffffff',
-
     },
     backgroundImage: {
-      logo: 'linear-gradient(125deg, #68D0F9 16.46%, #9ED7F3 36.98%, #D7BDF5 51.69%, #CC81F1 60.85%, #E96069 71.67%, #F09674 91.21%)',
+      logo: 'linear-gradient(125deg, #3db3e2 16.46%, #9ED7F3 36.98%, #D7BDF5 51.69%, #CC81F1 60.85%, #E96069 71.67%, #F09674 91.21%)',
     },
     fontFamily: {
       josefin: ['Josefin Sans', 'sans-serif'],
@@ -29,6 +29,8 @@ const config: Config = {
       '16': '16px',
       '18': '18px',
       '30': '30px',
+      '36': '36px',
+      '60': '60px',
     },
     fontWeight: {
       400: '400',
@@ -36,6 +38,7 @@ const config: Config = {
       600: '600',
       700: '700',
     },
+  },
   },
   plugins: [],
 };
