1c0aa649e808c7c3ee0a8925618715be7c30c3be
lvan-ni <g.nilvan@icloud.com>
2023-11-09 17:26:50 +0100

    Add back button, logo, and GitHub login option to login page
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,44 @@
 'use client';
 import React from 'react';
 import { signIn } from 'next-auth/react';
+import Link from 'next/link';
+import Image from 'next/image';
+import back from '@/assets/nav/back.png';
+import github from '@/assets/login/github.png';
 
 const Login = () => {
   return (
-    <div className='h-screen flex flex-col items-center justify-center gap-10'>
-      <button
-        className='w-1/2 h-10 border border-charcoal'
-        onClick={() => signIn('github', { callbackUrl: '/home' })}
-      >
-        Continue with GitHub
-      </button>
-      {/* <button className='w-1/2 h-10 border border-charcoal'>
+    <div className='flex flex-col mx-10 my-10 gap-10'>
+      <header className='flex justify-start'>
+        <Link href={'/'}>
+        <Image
+          src={back}
+          alt='back'
+          width={30}
+          height={30}
+        ></Image>
+        </Link>
+      </header>
+      <div className='flex flex-col items-center gap-36'>
+        <div className='flex flex-col gap-10'>
+          <h1 className='title-logo ombre-text'>applaudify</h1>
+          <h3 className='sub-title text-center text-charcoal'>
+            login / signup
+          </h3>
+        </div>
+        <button
+          className='button login-btn'
+          onClick={() => signIn('github', { callbackUrl: '/home' })}
+        >
+          <Image
+            src={github}
+            alt='github'
+            width={20}
+            height={20}
+          ></Image>
+          Continue with GitHub
+        </button>
+        {/* <button className='w-1/2 h-10 border border-charcoal'>
         Continue with Google
       </button>
       <button
@@ -20,6 +47,7 @@ const Login = () => {
       >
         Continue with LinkedIn
       </button> */}
+      </div>
     </div>
   );
 };
