29330b2761a9206b17d009f27f99e3cd0f455609
lvan-ni <g.nilvan@icloud.com>
2023-11-10 12:13:40 +0100

    Refactor Vanessa page component
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/applauds/vanessa/page.tsx
+++ src/app/applauds/vanessa/page.tsx
@@ -1,45 +1,53 @@
-"use client";
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { useSession } from "next-auth/react";
+'use client';
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import back from '@/assets/nav/back.png';
 
 const Vanessa = () => {
   const { data: session } = useSession();
 
-  const firstName = session?.user?.name?.split(" ")[0];
+  const firstName = session?.user?.name?.split(' ')[0];
   return (
-    <div className="flex flex-col mx-10 mt-14 gap-10">
-      <header className="flex justify-between">
-        <Link href="/applauds">←</Link>
-        <h4>Applaud from Vanessa</h4>
+    <div className='flex flex-col mx-10 mt-14 gap-10'>
+      <header className='flex justify-between items-center'>
+        <Link href='/applauds'>
+          <Image
+            src={back}
+            alt='back'
+            width={30}
+            height={30}
+          ></Image>
+        </Link>
+        <h4 className='body-main'>Applaud from Vanessa</h4>
         <div></div>
       </header>
-      <main className="flex flex-col items-center gap-10">
-        <section className="flex flex-col gap-4 border-solid border border-metal p-4">
-          <article className="flex items-center gap-6 p-2 border-solid border border-stone">
-            <Image
-              src="https://avatars.githubusercontent.com/u/101557392?v=4"
-              alt="Sender Profile"
-              width={50}
-              height={50}
-              className="rounded-full"
-            ></Image>
-            <div className="flex flex-col">
-              <h4 className="text-sm">Vanessa Wingårdh</h4>
-              <p className="text-xs">Developer Team Lead</p>
-              <p className="text-xs">DesignWeb Ltd.</p>
-            </div>
-          </article>
-          <p className="text-center">
-            &apos;{firstName} is a talented and dedicated software developer,
-            and I am grateful for their hard work and dedication. It was a
-            pleasure working with you.&apos;
-          </p>
-        </section>
-        <p className="text-center border-solid border border-metal p-2">
-          Applaud Published
-        </p>
+      <main className='flex flex-col items-center'>
+        <div className='single-applaud-border mt-14'>
+          <section className='flex flex-col gap-8 single-applaud small px-4 py-8 p-2'>
+            <article className='flex items-center bg-white gap-8'>
+              <Image
+                src='https://avatars.githubusercontent.com/u/101557392?v=4'
+                alt='Sender Profile'
+                width={61}
+                height={61}
+                className='rounded-full'
+              ></Image>
+              <div className='flex flex-col'>
+                <h4 className='name bg-white'>Vanessa Wingårdh</h4>
+                <p className='title-company bg-white'>Developer Team Lead</p>
+                <p className='title-company bg-white'>DesignWeb Ltd.</p>
+              </div>
+            </article>
+            <p className='body-main bg-white'>
+              &apos;{firstName} is a talented and dedicated software developer,
+              and I am grateful for their hard work and dedication. It was a
+              pleasure working with you.&apos;
+            </p>
+          </section>
+        </div>
+        <p className='text-center button mt-20'>Applaud Published</p>
       </main>
     </div>
   );
