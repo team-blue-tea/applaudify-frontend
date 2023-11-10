18057e332830587c3086243c62c7207c3059447f
lvan-ni <g.nilvan@icloud.com>
2023-11-10 12:13:47 +0100

    Refactor imports and remove unused state
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,21 +1,16 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import {
-  getAllApplauds,
-  getAllMembers,
-  addNewMember,
-  getNumberOfUnreadApplaudsByMemberEmail,
-} from '@/libs/DB';
+import { getAllApplauds, getAllMembers, addNewMember } from '@/libs/DB';
 import { useSession } from 'next-auth/react';
 import { ApplaudT } from '@/types/ApplaudT';
 import { MemberT } from '@/types/MemberT';
 import ApplaudCard from '@/components/ApplaudCard/ApplaudCard';
+import Inbox from '@/components/Inbox/Inbox';
 
 const Home = () => {
   const [applauds, setApplauds] = useState<ApplaudT[]>([]);
   const { data: session } = useSession();
-  const [notifications, setNotifications] = useState<string>('');
 
   useEffect(() => {
     if (!session) return;
@@ -43,52 +38,25 @@ const Home = () => {
     };
   }, [session]);
 
-  useEffect(() => {
-    (async () => {
-      const memberEmail = session?.user?.email;
-      if (memberEmail) {
-        const unreadNotifications =
-          await getNumberOfUnreadApplaudsByMemberEmail(memberEmail as string);
-        if (unreadNotifications !== 0) {
-          setNotifications(unreadNotifications);
-        }
-      }
-    })();
-  }, [session]);
-
   return (
     <div className='flex flex-col mx-10 mt-14 gap-10'>
-      <header className='flex flex-col items-center gap-10'>
+      <header className='flex w-full items-center justify-between'>
         <Link href='/home'>
           <h1 className='header-logo ombre-text'>applaudify</h1>
         </Link>
-        <div className='flex justify-between w-full items-center'>
-          <Link
-            href='/menu'
-            className='header-nav'
-          >
-            Menu
-          </Link>
-          <div className='flex gap-5 items-center'>
-            <Link
-              href='/profile'
-              className='header-nav'
-            >
-              Profile
-            </Link>
-            <Link
-              href='/applauds'
-              className='flex gap-2 items-center header-nav header-btn'
-            >
-              Inbox
-              {notifications !== '' && (
-                <div className='counter-border'>
-                  <div className='counter small'>{notifications}</div>
-                </div>
-              )}
-            </Link>
-          </div>
-        </div>
+        <Link
+          href='/menu'
+          className='header-nav'
+        >
+          Menu
+        </Link>
+        <Link
+          href='/profile'
+          className='header-nav'
+        >
+          Profile
+        </Link>
+        <Inbox session={session} />
       </header>
       <main className='flex flex-col gap-8 mt-6'>
         <ApplaudCard applauds={applauds} />
