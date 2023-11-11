b0e4e4a233ba04be4f7377afc78f63af53bfe4a7
lvan-ni <g.nilvan@icloud.com>
2023-11-11 11:08:49 +0100

    Remove unused Link import and add new components to Profile page
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { getAllMembers, getPublishedApplauds } from '@/libs/DB';
@@ -8,23 +7,25 @@ import { ApplaudT } from '@/types/ApplaudT';
 import { motion, AnimatePresence } from 'framer-motion';
 import { initialTabs as tabs } from '@/types/Tabs';
 import { MemberT } from '@/types/MemberT';
-import Inbox from '@/components/Inbox/Inbox';
 import ApplaudCard from '@/components/ApplaudCard/ApplaudCard';
 import arrow from '@/assets/card/arrow.png';
+import AppHeader from '@/components/AppHeader/AppHeader';
+import MockAppluadCards from '@/components/MockAppluadCards/MockAppluadCards';
 
 const Profile = () => {
   const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const [clicked, setClicked] = useState(false);
   const [activeTab, setActiveTab] = useState('Bio');
   const [member, setMember] = useState<MemberT>();
   const [publishedApplauds, setPublishedApplauds] = useState<ApplaudT[]>([]);
   const { data: session } = useSession();
-  
 
-  const firstName = session?.user?.name?.split(' ')[0];
+  const firstName = session?.user?.name?.split(' ')[0] as string;
   const imageURL = session?.user?.image as string;
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
+    setClicked(true);
   };
 
   useEffect(() => {
@@ -43,34 +44,19 @@ const Profile = () => {
 
   return (
     <div className='flex flex-col mx-10 mt-14 gap-10'>
-      <header className='flex w-full items-center justify-between'>
-        <Link href='/home'>
-          <h1 className='header-logo ombre-text'>applaudify</h1>
-        </Link>
-        <Link
-          href='/menu'
-          className='header-nav'
-        >
-          Menu
-        </Link>
-        <Link
-          href='/profile'
-          className='header-nav'
-        >
-          Profile
-        </Link>
-        {session && <Inbox session={session} />}
-      </header>
+      <AppHeader />
       <main className='flex flex-col items-center gap-10'>
         <section className='flex flex-col gap-8 items-center w-full'>
           <div className='flex items-center justify-center w-full gap-8 px-2 py-3'>
-            {session && <Image
-              src={imageURL}
-              alt='Profile photo'
-              width={88}
-              height={88}
-              className='rounded-full border border-silver'
-            ></Image>}
+            {session && (
+              <Image
+                src={imageURL}
+                alt='Profile photo'
+                width={88}
+                height={88}
+                className='rounded-full border border-silver'
+              ></Image>
+            )}
             <div className='w-3/5'>
               <h4 className='body-large'>{member?.name}</h4>
               <p className='body-small'>{member?.jobTitle}</p>
@@ -83,8 +69,8 @@ const Profile = () => {
                 key={item.label}
                 className={`${
                   item === selectedTab
-                    ? 'selected bg-metal button text-paper'
-                    : ''
+                    ? 'profile-button clicked'
+                    : 'profile-button'
                 } w-24 p-2 border border-silver rounded-3xl`}
                 onClick={() => {
                   handleTabClick(item.label);
@@ -170,148 +156,13 @@ const Profile = () => {
           </div>
         </section>
 
-        {/* PUBLISHED CARDS */}
-
-        <section className='flex flex-col items-center w-full'>
-          <h3 className='sub-title'>applauds</h3>
+        <h3 className='sub-title'>{firstName}&apos;s applauds</h3>
+        <section className='flex flex-col w-full'>
           <ApplaudCard applauds={publishedApplauds} />
-        </section>
-
-        {/* 1ST CARD */}
-
-        <section className='applaud-card'>
-          <article className='sender-name-card'>
-            <div className='flex flex-col text-right gap-1.5 bg-white'>
-              <h4 className='name bg-white'>Hugo Dahlgren</h4>
-              <p className='title-company bg-white'>Senior Software Engineer</p>
-              <p className='title-company bg-white'>Tech Solutions Inc.</p>
-            </div>
-            <Image
-              src='https://avatars.githubusercontent.com/u/91157834?v=4'
-              alt='Sender Profile'
-              width={58}
-              height={58}
-              className='profile-img'
-            ></Image>
-          </article>
-          <Image
-            src={arrow}
-            alt='Arrow'
-            width={14}
-            height={16}
-            className='self-center bg-white'
-          ></Image>
-          <article className='receiver-name-card'>
-          {session && <Image
-              src={imageURL}
-              alt='Receiver Profile'
-              width={58}
-              height={58}
-              className='profile-img'
-            ></Image>}
-            <div className='flex flex-col gap-1.5 bg-paper'>
-              <h4 className='name bg-paper'>{session?.user?.name}</h4>
-              <p className='title-company bg-paper'>Fullstack Developer</p>
-              <p className='title-company bg-paper'>Freelance</p>
-            </div>
-          </article>
-          <p className='text-center pt-5 body-main bg-white'>
-            &apos;{firstName} is a great developer! I really enjoyed working
-            with {firstName} on this project. {firstName} is super easy to work
-            with and making decisions together is a breeze. Looking forward to
-            work with you again in the future.&apos;
-          </p>
-        </section>
-
-        {/* 2ND CARD */}
-
-        <section className='applaud-card'>
-          <article className='sender-name-card'>
-            <div className='name-card-spacing text-right bg-white'>
-              <h4 className='name bg-white'>Vanessa Wingårdh</h4>
-              <p className='title-company bg-white'>Developer Team Lead</p>
-              <p className='title-company bg-white'>DesignWeb Ltd.</p>
-            </div>
-            <Image
-              src='https://avatars.githubusercontent.com/u/101557392?v=4'
-              alt='Sender Profile'
-              width={58}
-              height={58}
-              className='profile-img'
-            ></Image>
-          </article>
-          <Image
-            src={arrow}
-            alt='Arrow'
-            width={14}
-            height={16}
-            className='self-center bg-white'
-          ></Image>
-          <article className='receiver-name-card'>
-            {session && <Image
-              src={imageURL}
-              alt='Receiver Profile'
-              width={58}
-              height={58}
-              className='profile-img'
-            ></Image>}
-            <div className='name-card-spacing bg-paper'>
-              <h4 className='name  bg-paper'>{session?.user?.name}</h4>
-              <p className='title-company  bg-paper'>Fullstack Developer</p>
-              <p className='title-company  bg-paper'>Freelance</p>
-            </div>
-          </article>
-          <p className='text-center pt-5 body-main bg-white'>
-            &apos;{firstName} is a talented and dedicated software developer,
-            and I am grateful for their hard work and dedication. It was a
-            pleasure working with you.&apos;
-          </p>
-        </section>
-
-        {/* 3RDCARD */}
-
-        <section className='applaud-card'>
-          <article className='sender-name-card'>
-            <div className='name-card-spacing text-right bg-white'>
-              <h4 className='name bg-white'>Muhammad Ahsan Ayaz</h4>
-              <p className='title-company bg-white'>Principal Engineer</p>
-              <p className='title-company bg-white'>Airbnb</p>
-            </div>
-            <Image
-              src='https://avatars.githubusercontent.com/u/9844254?v=4'
-              alt='Sender Profile'
-              width={58}
-              height={58}
-              className='profile-img bg-white'
-            ></Image>
-          </article>
-          <Image
-            src={arrow}
-            alt='Arrow'
-            width={14}
-            height={16}
-            className='self-center bg-white'
-          ></Image>
-          <article className='receiver-name-card'>
-           {session && <Image
-              src={imageURL}
-              alt='Receiver Profile'
-              width={58}
-              height={58}
-              className='profile-img'
-            ></Image>}
-            <div className='name-card-spacing bg-paper'>
-              <h4 className='name bg-paper'>{session?.user?.name}</h4>
-              <p className='title-company bg-paper'>Fullstack Developer</p>
-              <p className='title-company bg-paper'>Freelance</p>
-            </div>
-          </article>
-          <p className='text-center pt-5 body-main bg-white'>
-            &apos;Working along side {firstName} on various projects has been
-            nothing short of delightful. {firstName} has a keen eye for detail
-            and profound understanding of user experience, which had an immense
-            impact on the outcome of the project.&apos;
-          </p>
+          <MockAppluadCards
+            firstName={firstName}
+            imageURL={imageURL}
+          />
         </section>
       </main>
     </div>
