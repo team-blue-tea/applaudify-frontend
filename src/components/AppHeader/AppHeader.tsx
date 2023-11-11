ec213dc17e3f646133aa7b075d3419aa773a8ced
lvan-ni <g.nilvan@icloud.com>
2023-11-11 11:09:09 +0100

    Add AppHeader component with updated design and functionality
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,58 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import Inbox from '@/components/Inbox/Inbox';
+import  Menu  from '@/components/Menu/Menu';
+
+const AppHeader = () => {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <header className='flex flex-col justify-between items-center gap-3'>
+        <div className='flex w-full items-center justify-between'>
+          <Link href='/home'>
+            <h1 className='header ombre-text'>applaudify</h1>
+          </Link>
+          <div className='flex items-center gap-3'>
+            {/* <Link
+              href='/menu'
+              className='header-nav'
+            >
+              Menu
+            </Link> */}
+            <Menu />
+            {session && <Inbox session={session} />}
+          </div>
+        </div>
+        <button className='search-btn w-full text-silver'>Search</button>
+      </header>
+
+      {/* // OLD */}
+      {/* <header className='flex flex-col justify-between items-center gap-3'>
+        <div className='flex w-full items-center justify-between'>
+          {' '}
+          <Link href='/home'>
+            <h1 className='header-logo ombre-text'>applaudify</h1>
+          </Link>
+          <Link
+            href='/menu'
+            className='header-nav'
+          >
+            Menu
+          </Link>
+          <Link
+            href='/profile'
+            className='header-nav'
+          >
+            Profile
+          </Link>
+          {session && <Inbox session={session} />}
+        </div>
+      </header> */}
+    </>
+  );
+};
+
+export default AppHeader;
