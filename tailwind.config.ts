0548d1ce9e761c84638b3f7d20007b169798f85a
lvan-ni <g.nilvan@icloud.com>
2023-11-11 11:07:55 +0100

    Update Tailwind config with new colors, background image, and font styles
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,48 +1,47 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-    colors: {
-      black: '#000000',
-      metal: '#0d0d0d',
-      charcoal: '#303030',
-      stone: '#5a5a5a',
-      gray: '#919191', 
-      silver: '#b8b8b8',
-      light: '#f6f6f6',
-      paper: '#f8f6f2',
-      white: '#ffffff',
+      colors: {
+        black: '#000000',
+        metal: '#0d0d0d',
+        charcoal: '#303030',
+        stone: '#5a5a5a',
+        gray: '#919191',
+        silver: '#b8b8b8',
+        light: '#f6f6f6',
+        paper: '#f8f6f2',
+        white: '#ffffff',
+        blue: '#3db3e2',
+      },
+      backgroundImage: {
+        logo: 'linear-gradient(125deg, #3db3e2 16.46%, #9ED7F3 36.98%, #D7BDF5 51.69%, #CC81F1 60.85%, #E96069 71.67%, #F09674 91.21%)',
+      },
+      fontFamily: {
+        josefin: ['Josefin Sans', 'sans-serif'],
+        avenir: ['Avenir Next', 'sans-serif'],
+      },
+      fontSize: {
+        '10': '10px',
+        '12': '12px',
+        '14': '14px',
+        '16': '16px',
+        '18': '18px',
+        '20': '20px',
+        '24': '24px',
+        '30': '30px',
+        '36': '36px',
+        '60': '60px',
+      },
+      fontWeight: {
+        400: '400',
+        500: '500',
+        600: '600',
+        700: '700',
+      },
     },
-    backgroundImage: {
-      logo: 'linear-gradient(125deg, #3db3e2 16.46%, #9ED7F3 36.98%, #D7BDF5 51.69%, #CC81F1 60.85%, #E96069 71.67%, #F09674 91.21%)',
-    },
-    fontFamily: {
-      josefin: ['Josefin Sans', 'sans-serif'],
-      avenir: ['Avenir Next', 'sans-serif'],
-    },
-    fontSize: {
-      '10': '10px',
-      '12': '12px',
-      '14': '14px',
-      '16': '16px',
-      '18': '18px',
-      '20': '20px',
-      '24': '24px',
-      '30': '30px',
-      '36': '36px',
-      '60': '60px',
-    },
-    fontWeight: {
-      400: '400',
-      500: '500',
-      600: '600',
-      700: '700',
-    },
-  },
   },
   plugins: [],
 };
