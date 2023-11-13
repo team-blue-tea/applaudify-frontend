8988cd8f52c7c09a55bcddca7e0973798a9e04f1
lvan-ni <g.nilvan@icloud.com>
2023-11-13 16:14:07 +0100

    Refactor about page with new design and components
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,107 +1,83 @@
 'use client';
 import React from 'react';
-import { useSession, signOut } from 'next-auth/react';
+import Header from '@/components/Header/Header';
 import Image from 'next/image';
 import Link from 'next/link';
 import ilija from '@/assets/about/ilija.png';
 import lvan from '@/assets/about/lvan.png';
 import tim from '@/assets/about/tim.png';
 import sudha from '@/assets/about/sudha.png';
-import logo from '@/assets/logo.png';
 
 const About = () => {
-  const { data: session } = useSession();
+
   return (
-    <div className='landingPage'>
-      <header>
-        <Link href='/'>
-          <Image
-            src={logo}
-            alt='logo'
-            width={36}
-            height={18}
-          ></Image>
-        </Link>
-        <Link href='/about'>About</Link>
-        <Link href='https://github.com/orgs/team-blue-tea/repositories'>
-          Docs
-        </Link>
-        {!session ? (
-          <Link href='/login'>
-            <button className='headerLogin'>Login</button>
+    <div className='flex flex-col items-center mb-10'>
+      <Header />
+      <main className='flex flex-col gap-20 mt-20 mx-10'>
+        <section className='flex flex-col items-center gap-10'>
+          <h3 className='sub-title'>About Us</h3>
+          <p className='body-small'>
+            At Applaudify, we understand that for freelancers and consultants,
+            every project is a testament to their adaptability, creativity, and
+            commitment. Hence, our platform doesn&apos;t just stop at collecting
+            affirmations; it transforms them into a dynamic journey. This not
+            only serves as a source of personal motivation but also as a
+            showcase for potential clients, demonstrating the value and impact
+            these professionals bring to the table.
+          </p>
+        </section>
+        <section className='flex flex-col items-center gap-10'>
+          <h3 className='sub-title'>Our Team</h3>
+          <p className='body-small'>
+            Behind Applaudify is a team of dedicated individuals, each bringing
+            a unique set of skills and experiences to the table:
+          </p>
+        </section>
+        <section className='grid grid-cols-2 gap-10'>
+          <Link href='https://www.linkedin.com/in/lvan-ni/'>
+            <article className='flex flex-col items-center'>
+              <Image
+                src={lvan}
+                alt='Lvan Photo'
+                width={100}
+              ></Image>
+              <h4>Lvan Ni</h4>
+            </article>
           </Link>
-        ) : (
-          <button
-            className='headerLogin'
-            onClick={() => signOut({ callbackUrl: '/' })}
-          >
-            Sign out
-          </button>
-        )}
-      </header>
-      <section className='flex flex-col gap-10'>
-        <h3>About Us</h3>
-        <p>
-          At Applaudify, we understand that for freelancers and consultants,
-          every project is a testament to their adaptability, creativity, and
-          commitment. Hence, our platform doesn&apos;t just stop at collecting
-          affirmations; it transforms them into a dynamic journey. This not only
-          serves as a source of personal motivation but also as a showcase for
-          potential clients, demonstrating the value and impact these
-          professionals bring to the table.
-        </p>
-      </section>
-      <section className='flex flex-col gap-10'>
-        <h3>Our Team</h3>
-        <p>
-          Behind Applaudify is a team of dedicated individuals, each bringing a
-          unique set of skills and experiences to the table:
-        </p>
-      </section>
-      <section className='grid grid-cols-2 gap-10'>
-        <Link href='https://www.linkedin.com/in/lvan-ni/'>
-          <article className='flex flex-col items-center'>
-            <Image
-              src={lvan}
-              alt='Lvan Photo'
-              width={100}
-            ></Image>
-            <h4>Lvan Ni</h4>
-          </article>
-        </Link>
-        <Link href='https://www.linkedin.com/in/ilijakrilovic/'>
-          <article className='flex flex-col items-center'>
-            <Image
-              src={ilija}
-              alt='ilija Photo'
-              width={100}
-            ></Image>
-            <h4>Ilija Krilovic</h4>
-          </article>
-        </Link>
-        <Link href='https://www.linkedin.com/in/tim-hansson-meng/'>
-          <article className='flex flex-col items-center'>
-            <Image
-              src={tim}
-              alt='tim Photo'
-              width={100}
-            ></Image>
-            <h4 className='text-center'>Tim Hansson Meng</h4>
-          </article>
-        </Link>
-        <Link href='https://www.linkedin.com/in/sudha-madhuri-poojari/'>
-          <article className='flex flex-col items-center'>
-            <Image
-              src={sudha}
-              alt='Suhda Photo'
-              width={100}
-            ></Image>
-            <h4 className='text-center'>Sudha Madhuri Poojari</h4>
-          </article>
-        </Link>
-      </section>
-      <footer>
-        <p>© 2023 Applaudify</p>
+          <Link href='https://www.linkedin.com/in/ilijakrilovic/'>
+            <article className='flex flex-col items-center'>
+              <Image
+                src={ilija}
+                alt='ilija Photo'
+                width={100}
+              ></Image>
+              <h4>Ilija Krilovic</h4>
+            </article>
+          </Link>
+          <Link href='https://www.linkedin.com/in/tim-hansson-meng/'>
+            <article className='flex flex-col items-center'>
+              <Image
+                src={tim}
+                alt='tim Photo'
+                width={100}
+              ></Image>
+              <h4 className='text-center'>Tim Hansson Meng</h4>
+            </article>
+          </Link>
+          <Link href='https://www.linkedin.com/in/sudha-madhuri-poojari/'>
+            <article className='flex flex-col items-center'>
+              <Image
+                src={sudha}
+                alt='Suhda Photo'
+                width={100}
+              ></Image>
+              <h4 className='text-center'>Sudha Madhuri Poojari</h4>
+            </article>
+          </Link>
+        </section>
+      </main>
+      <footer className='flex flex-col gap-10 mt-20 mx-10'>
+        <p className='small text-center text-stone mt-20'>© 2023 Applaudify</p>
       </footer>
     </div>
   );
