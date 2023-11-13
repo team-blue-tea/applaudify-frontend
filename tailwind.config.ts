8718bd1bfa230ae8795c77a73a28479dfe8fd055
lvan-ni <g.nilvan@icloud.com>
2023-11-13 16:12:53 +0100

    Add new colors and font sizes to tailwind.config.ts.
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
         paper: '#f8f6f2',
         white: '#ffffff',
         blue: '#3db3e2',
+        hdr: '#f6f6f67a',
       },
       backgroundImage: {
         logo: 'linear-gradient(125deg, #3db3e2 16.46%, #9ED7F3 36.98%, #D7BDF5 51.69%, #CC81F1 60.85%, #E96069 71.67%, #F09674 91.21%)',
@@ -24,6 +25,7 @@ const config: Config = {
         avenir: ['Avenir Next', 'sans-serif'],
       },
       fontSize: {
+        '8': '8px',
         '10': '10px',
         '12': '12px',
         '14': '14px',
@@ -40,6 +42,14 @@ const config: Config = {
         500: '500',
         600: '600',
         700: '700',
+      },
+      backgroundOpacity: {
+        '10': '0.1',
+        '25': '0.25',
+        '50': '0.5',
+      },
+      backdropBlur: {
+        xs: '2px',
       },
     },
   },
