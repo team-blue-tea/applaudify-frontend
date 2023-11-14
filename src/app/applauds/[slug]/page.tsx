a532c3f164adee3f0ee651af379e9640d1a58de9
lvan-ni <g.nilvan@icloud.com>
2023-11-14 09:18:21 +0100

    Refactor SingleApplaud component layout
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/applauds/[slug]/page.tsx
+++ src/app/applauds/[slug]/page.tsx
@@ -15,49 +15,51 @@ const SingleApplaud = async ({ params }: { params: { slug: string } }) => {
   const { sender, comment, published } = filteredApplaud[0];
 
   return (
-      <div className='flex flex-col mx-10 mt-14 gap-10'>
-        <header className='flex justify-between items-center'>
-          <Link href='/applauds'>
-            <Image
-              src={back}
-              alt='back'
-              width={30}
-              height={30}
-            ></Image>
-          </Link>
-          <h4 className='body-main'>Applaud from {sender.name.split(' ')[0]}</h4>
-          <div></div>
-        </header>
-        <main className='flex flex-col items-center'>
-          <div className='single-applaud-border mt-14'>
-            <section className='flex flex-col gap-8 single-applaud small px-4 py-8 p-2'>
-              <article className='flex items-center bg-white gap-8'>
-                <Image
-                  src={sender.avatarUrl}
-                  alt='Sender Profile'
-                  width={61}
-                  height={61}
-                  className='rounded-full'
-                ></Image>
-                <div className='flex flex-col'>
-                  <h4 className='name bg-white'>{sender.name}</h4>
-                  <p className='title-company bg-white'>{sender.jobTitle}</p>
-                  <p className='title-company bg-white'>{sender.company}</p>
-                </div>
-              </article>
-              <p className='body-main bg-white'>{comment}</p>
-            </section>
+    <div className='flex flex-col mx-10 mt-14 gap-10'>
+      <header className='flex justify-between items-center'>
+        <Link href='/applauds'>
+          <Image
+            src={back}
+            alt='back'
+            width={30}
+            height={30}
+          ></Image>
+        </Link>
+        <h4 className='body-main'>Applaud from {sender.name.split(' ')[0]}</h4>
+        <div></div>
+      </header>
+      <main className='flex flex-col items-center'>
+        <div className='single-applaud-border mt-14'>
+          <section className='flex flex-col gap-8 single-applaud small px-4 py-8 p-2'>
+            <article className='flex items-center bg-white gap-8'>
+              <Image
+                src={sender.avatarUrl}
+                alt='Sender Profile'
+                width={61}
+                height={61}
+                className='rounded-full'
+              ></Image>
+              <div className='flex flex-col'>
+                <h4 className='name bg-white'>{sender.name}</h4>
+                <p className='title-company bg-white'>{sender.jobTitle}</p>
+                <p className='title-company bg-white'>{sender.company}</p>
+              </div>
+            </article>
+            <p className='body-main bg-white'>{comment}</p>
+          </section>
+        </div>
+        {published ? (
+          <div className='flex flex-col items-center w-full'>
+            <p className='text-center button mt-10'>Applaud Published!</p>
+            <UnpublishButton slug={slug} />
           </div>
-          {published ? (
-            <div className='flex flex-col items-center'>
-              <p className='text-center button mt-10'>Applaud Published!</p>
-              <UnpublishButton slug={slug} />
-            </div>
-          ) : (
+        ) : (
+          <div className='flex flex-col items-center justify-center w-full'>
             <PublishButton slug={slug} />
-          )}
-        </main>
-      </div>
+          </div>
+        )}
+      </main>
+    </div>
   );
 };
 
