ab3debdfb2926e3417489afe0ac29d4396273936
lvan-ni <g.nilvan@icloud.com>
2023-11-14 17:48:08 +0100

    Add initial render check and loading status to useSession hook.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 const Dashboard = () => {
-  const { data: session } = useSession();
+  const [isInitialRender, setIsInitialRender] = useState(true);
+  const { data: session, status } = useSession();
   const router = useRouter();
 
   useEffect(() => {
+    if (isInitialRender) {
+      setIsInitialRender(false);
+      return;
+    }
+
+    if (status === 'loading') {
+      return;
+    }
+
     if (session) {
       router.push('/home');
     } else {
       router.push('/landing');
     }
-    [session, router];
-  });
+  }, [isInitialRender, status, session, router]);
 
   return <div className='bg-light w-full h-screen'></div>;
 };
