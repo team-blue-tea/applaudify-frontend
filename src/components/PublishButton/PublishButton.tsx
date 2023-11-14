4219458126f5a686a8f94ef9bdf52052f746d71f
lvan-ni <g.nilvan@icloud.com>
2023-11-14 09:18:28 +0100

    Add animation and refresh functionality to PublishButton component
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/components/PublishButton/PublishButton.tsx
+++ src/components/PublishButton/PublishButton.tsx
@@ -1,26 +1,123 @@
 'use client';
 import React from 'react';
 import { setApplaudPublished } from '@/libs/DB';
+import { useAnimate } from 'framer-motion';
+import { stagger } from 'framer-motion';
+import { animate } from 'framer-motion';
+import { useRouter } from 'next/navigation';
+
+type AnimationSequence = Parameters<typeof animate>[0];
 
 type PublishButtonProps = {
   slug: string;
 };
 
+const randomNumberBetween = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 const PublishButton: React.FC<PublishButtonProps> = ({ slug }) => {
+  const router = useRouter();
+  const [scope, animate] = useAnimate();
+
   const handlePublishClick = () => {
     setApplaudPublished(slug);
     setTimeout(() => {
-      window.location.href = '/applauds/published';
-    }, 500);
+      router.refresh();
+      window.location.reload();
+    }, 300);
+
+    const sparkles = Array.from({ length: 20 });
+    const sparklesAnimation: AnimationSequence = sparkles.map((_, index) => [
+      `.sparkle-${index}`,
+      {
+        x: randomNumberBetween(-100, 100),
+        y: randomNumberBetween(-100, 100),
+        scale: randomNumberBetween(1.5, 2.5),
+        opacity: 1,
+      },
+      {
+        duration: 0.4,
+        at: '<',
+      },
+    ]);
+    const sparklesFadeOut = sparkles.map((_, index) => [
+      `.sparkle-${index}`,
+      {
+        opacity: 0,
+        scale: 0,
+      },
+      {
+        duration: 0.3,
+        at: '<',
+      },
+    ]);
+    const sparklesReset: AnimationSequence = sparkles.map((_, index) => [
+      `.sparkle-${index}`,
+      {
+        x: 0,
+        y: 0,
+      },
+      {
+        duration: 0.000001,
+      },
+    ]);
+
+    animate([
+      ...sparklesReset,
+      ['.letter', { y: -32 }, { duration: 0.2, delay: stagger(0.05) }],
+      ['button', { scale: 0.8 }, { duration: 0.1, at: '<' }],
+      ['button', { scale: 1 }, { duration: 0.1 }],
+      ...sparklesAnimation,
+      ['.letter', { y: 0 }, { duration: 0.000001 }],
+      ...sparklesFadeOut,
+    ]);
   };
 
   return (
-    <button
-      className='btn mt-20'
-      onClick={handlePublishClick}
+    <div
+      ref={scope}
+      className='flex justify-center w-full'
     >
-      Publish
-    </button>
+      <button
+        className='relative border border-silver rounded-3xl px-5 py-2.5 w-2/3 mt-20'
+        onClick={handlePublishClick}
+      >
+        <span className='sr-only'>Publish</span>
+        <span
+          className='block h-8 overflow-hidden'
+          aria-hidden
+        >
+          {['P', 'u', 'b', 'l', 'i', 's', 'h'].map((letter, index) => (
+            <span
+              data-letter={letter}
+              className='letter relative inline-block h-8 leading-8
+              after:absolute after:left-0 after:top-full after:h-8
+              after:content-[attr(data-letter)]'
+              key={`${letter}-${index}`}
+            >
+              {letter}
+            </span>
+          ))}
+        </span>
+        <span
+          aria-hidden
+          className='pointer-events-none absolute inset-0 -z-10 block'
+        >
+          {Array.from({ length: 20 }).map((_, index) => (
+            <svg
+              className={`absolute fill-blue left-1/2 top-1/2 opacity-0 sparkle-${index}`}
+              key={index}
+              viewBox='0 0 122 117'
+              width='10'
+              height='10'
+            >
+              <path d='M64.39,2,80.11,38.76,120,42.33a3.2,3.2,0,0,1,1.83,5.59h0L91.64,74.25l8.92,39a3.2,3.2,0,0,1-4.87,3.4L61.44,96.19,27.09,116.73a3.2,3.2,0,0,1-4.76-3.46h0l8.92-39L1.09,47.92A3.2,3.2,0,0,1,3,42.32l39.74-3.56L58.49,2a3.2,3.2,0,0,1,5.9,0Z' />
+            </svg>
+          ))}
+        </span>
+      </button>
+    </div>
   );
 };
 
