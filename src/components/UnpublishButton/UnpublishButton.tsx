1985dcbbe3420c8d2001c25f93522d37f1061b1f
lvan-ni <g.nilvan@icloud.com>
2023-11-14 09:18:34 +0100

    Fix UnpublishButton refresh issue
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/components/UnpublishButton/UnpublishButton.tsx
+++ src/components/UnpublishButton/UnpublishButton.tsx
@@ -2,29 +2,27 @@
 import React from 'react';
 import { setApplaudUnpublished } from '@/libs/DB';
 import { useRouter } from 'next/navigation';
-import { useAnimate } from "framer-motion";
 
 type PublishButtonProps = {
   slug: string;
 };
 
 const UnpublishButton: React.FC<PublishButtonProps> = ({ slug }) => {
-  const [scope, animate] = useAnimate();
   const router = useRouter();
   const handlePublishClick = () => {
     setApplaudUnpublished(slug);
-    router.refresh();
-    window.location.reload();
+    setTimeout(() => {
+      router.refresh();
+      window.location.reload();
+    }, 200);
   };
   return (
-    <div ref={scope}>
     <button
       className='btn mt-10'
       onClick={handlePublishClick}
     >
       Unpublish
     </button>
-    </div>
   );
 };
 
