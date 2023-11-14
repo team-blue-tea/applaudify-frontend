f29ffead825e445d8f7825f39da771a97a142e09
lvan-ni <g.nilvan@icloud.com>
2023-11-14 17:07:02 +0100

    Remove unused code and comments, and refactor imports in Home page component
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,29 +1,15 @@
-// 'use client';
-import React, {
-  useState,
-  useEffect,
-  ChangeEvent,
-  FormEvent,
-  useRef,
-} from 'react';
-import { getAllApplauds, getAllMembers, addNewMember } from '@/libs/DB';
-import { useSession } from 'next-auth/react';
-import { ApplaudT } from '@/types/ApplaudT';
-import { MemberT } from '@/types/MemberT';
+import { getAllApplauds, getAllMembers } from '@/libs/DB';
 import CardForHome from '@/components/CardForHome/CardForHome';
 import Header from '@/components/Header/Header';
 import NewMemberCheck from '@/components/NewMemberCheck/NewMemeberCheck';
-import SearchComponent from '@/components/Search/Search';
 
 const Home = async () => {
   const applauds = await getAllApplauds();
-  const members = await getAllMembers();
 
   return (
     <div className='flex flex-col gap-10 mt-8'>
       <Header />
       <NewMemberCheck />
-      {/* <SearchComponent /> */}
       <main className='flex flex-col gap-8 mt-1 mx-10'>
         <CardForHome applauds={applauds} />
       </main>
