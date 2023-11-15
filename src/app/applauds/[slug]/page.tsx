cb91609a238abdd775e4b963cb4e73b100c9c790
lvan-ni <g.nilvan@icloud.com>
2023-11-15 10:46:25 +0100

    Refactor code for improved readability and efficiency.

--- src/app/applauds/[slug]/page.tsx
+++ src/app/applauds/[slug]/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { getAllApplauds } from '@/libs/DB';
 import { ApplaudT } from '@/types/ApplaudT';
-import PublishButton from '@/components/PublishButton/PublishButton';
+import PublishButton from '@/components/Publish/PublishButton';
 import UnpublishButton from '@/components/UnpublishButton/UnpublishButton';
 import back from '@/assets/nav/back.png';
 
