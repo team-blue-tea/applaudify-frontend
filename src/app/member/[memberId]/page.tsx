c8985c66c91a9a99db88142c52fcd46fc09f6e23
lvan-ni <g.nilvan@icloud.com>
2023-11-15 08:25:06 +0100

    Refactor member profile page
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/member/[memberId]/page.tsx
+++ src/app/member/[memberId]/page.tsx
@@ -1,36 +1,44 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+// 'use client';
+// import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { getAllMembers, getPublishedApplauds } from '@/libs/DB';
 import Header from '@/components/Header/Header';
 import ProfileInfo from '@/components/ProfileInfo/ProfileInfo';
 import CardForProfile from '@/components/CardForProfile/CardForProfile';
 import MockAppluadCards from '@/components/MockAppluadCards/MockAppluadCards';
-import { ApplaudT } from '@/types/ApplaudT';
+// import { ApplaudT } from '@/types/ApplaudT';
 import { MemberT } from '@/types/MemberT';
 
-const MemberProfile = () => {
-  const [member, setMember] = useState<MemberT>();
-  const [individualApplauds, setIndividualApplauds] = useState<ApplaudT[]>([]);
+const MemberProfile = async ({ params }: { params: { memberId: string } }) => {
+  // const [member, setMember] = useState<MemberT>();
+  // console.log(member);
+  // const [individualApplauds, setIndividualApplauds] = useState<ApplaudT[]>([]);
+
+  const { memberId } = params;
+  const members: MemberT[] = await getAllMembers();
+  const member = members.find((member) => member.id === memberId);
+  const individualApplauds = await getPublishedApplauds(
+    member?.email as string
+  );
 
   const firstName = member?.name.split(' ')[0] as string;
   const fullName = member?.name as string;
 
-  useEffect(() => {
-    (async () => {
-      const urlString = window.location.href;
-      const urlObject = new URL(urlString);
-      const pathname = urlObject.pathname;
-      const parts = pathname.split('/');
-      const memberId = parts[parts.length - 1];
-      const members: MemberT[] = await getAllMembers();
-      setMember(members.find((member) => member.id === memberId));
-      const individualApplauds = await getPublishedApplauds(
-        member?.email as string
-      );
-      setIndividualApplauds(individualApplauds);
-    })();
-  }, [member]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const urlString = window.location.href;
+  //     const urlObject = new URL(urlString);
+  //     const pathname = urlObject.pathname;
+  //     const parts = pathname.split('/');
+  //     const memberId = parts[parts.length - 1];
+  //     const members: MemberT[] = await getAllMembers();
+  //     setMember(members.find((member) => member.id === memberId));
+  //     const individualApplauds = await getPublishedApplauds(
+  //       member?.email as string
+  //     );
+  //     setIndividualApplauds(individualApplauds);
+  //   })();
+  // }, [member]);
 
   return (
     <div className='flex flex-col mt-4 gap-10'>
