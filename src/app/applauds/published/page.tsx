0d6c4bfd1a4f1b8eb4e2a8ed1a41d785370e81a9
lvan-ni <g.nilvan@icloud.com>
2023-11-15 08:32:05 +0100

    Commented out unused code in published and unpublished pages

--- src/app/applauds/published/page.tsx
+++ src/app/applauds/published/page.tsx
@@ -1,29 +1,29 @@
-'use client';
-import { motion } from 'framer-motion';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+// 'use client';
+// import { motion } from 'framer-motion';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
 
-const Published = () => {
-  const router = useRouter();
+// const Published = () => {
+//   const router = useRouter();
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      router.back();
-    }, 2000);
-    return () => clearTimeout(timeoutId);
-  }, [router]);
+//   useEffect(() => {
+//     const timeoutId = setTimeout(() => {
+//       router.back();
+//     }, 2000);
+//     return () => clearTimeout(timeoutId);
+//   }, [router]);
 
-  return (
-    <motion.main
-      className='flex justify-center items-center w-screen h-screen'
-      animate={{
-        scale: [1, 2, 2, 1, 1],
-        rotate: [0, 0, 270, 270, 0],
-      }}
-    >
-      <motion.h2>Applaud published!</motion.h2>
-    </motion.main>
-  );
-};
+//   return (
+//     <motion.main
+//       className='flex justify-center items-center w-screen h-screen'
+//       animate={{
+//         scale: [1, 2, 2, 1, 1],
+//         rotate: [0, 0, 270, 270, 0],
+//       }}
+//     >
+//       <motion.h2>Applaud published!</motion.h2>
+//     </motion.main>
+//   );
+// };
 
-export default Published;
+// export default Published;
--- src/app/applauds/unpublished/page.tsx
+++ src/app/applauds/unpublished/page.tsx
@@ -1,24 +1,24 @@
-'use client';
-import React, {useEffect} from 'react';
-import { useRouter } from 'next/navigation';
+// 'use client';
+// import React, {useEffect} from 'react';
+// import { useRouter } from 'next/navigation';
 
-const Unpublished = () => {
+// const Unpublished = () => {
   
-  const router = useRouter();
+//   const router = useRouter();
 
-  useEffect(() => {
-    (() => {
-      setTimeout(() => {
-        router.back();
-      }, 1000);
-    })();
-  }, [router]);
+//   useEffect(() => {
+//     (() => {
+//       setTimeout(() => {
+//         router.back();
+//       }, 1000);
+//     })();
+//   }, [router]);
 
-  return (
-    <main className='flex justify-center items-center w-screen h-screen'>
-      <h2>Publish Reverted!</h2>
-    </main>
-  );
-};
+//   return (
+//     <main className='flex justify-center items-center w-screen h-screen'>
+//       <h2>Publish Reverted!</h2>
+//     </main>
+//   );
+// };
 
-export default Unpublished;
+// export default Unpublished;
