a823c6be5b649d9ce9e0d850d944dc64f61bb973
lvan-ni <g.nilvan@icloud.com>
2023-11-15 08:25:18 +0100

    Comment out unused code and refactor Inbox component
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/components/Inbox/Inbox.tsx
+++ src/components/Inbox/Inbox.tsx
@@ -1,26 +1,34 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
 import { getUnreadApplauds } from '@/libs/DB';
-import { SessionT } from '@/types/SessionT';
+// import { SessionT } from '@/types/SessionT';
 
-type InboxProps = {
-  session: SessionT;
-};
+// type InboxProps = {
+//   session: SessionT;
+// };
 
-const Inbox: React.FC<InboxProps> = ({ session }) => {
-  const [Numbers, setNumbers] = useState<string>('');
+// : React.FC<InboxProps> 
+// { session }
 
-  useEffect(() => {
-    (async () => {
-      const memberEmail = session?.user?.email;
-      if (memberEmail) {
-        const unreadNumbers = await getUnreadApplauds(memberEmail as string);
-        if (unreadNumbers !== 0) {
-          setNumbers(unreadNumbers);
-        }
-      }
-    })();
-  }, [session]);
+const Inbox = async () => {
+  // const [Numbers, setNumbers] = useState<string>('');
+  // useEffect(() => {
+  //   (async () => {
+  //     const memberEmail = session?.user?.email;
+  //     if (memberEmail) {
+  //       const unreadNumbers = await getUnreadApplauds(memberEmail as string);
+  //       if (unreadNumbers !== 0) {
+  //         setNumbers(unreadNumbers);
+  //       }
+  //     }
+  //   })();
+  // }, [session]);
+
+  const session = await getServerSession();
+  const memberEmail = session?.user?.email;
+  const unreadNumbers = await getUnreadApplauds(memberEmail as string);
+  const Numbers = unreadNumbers !== 0 ? unreadNumbers : '';
 
   return (
     <Link
