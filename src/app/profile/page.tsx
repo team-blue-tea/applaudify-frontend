0bbb3729a937494426fffdf91886f8d2b0926bce
lvan-ni <g.nilvan@icloud.com>
2023-11-15 08:25:11 +0100

    Comment out unused code and update session handling in Profile component
    Co-authored-by: Ilija Krilovic <ica1130@users.noreply.github.com>

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,8 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+// 'use client';
+// import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { useSession } from 'next-auth/react';
+// import { useSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
 import { getAllMembers, getPublishedApplauds } from '@/libs/DB';
 import Header from '@/components/Header/Header';
 import CardForProfile from '@/components/CardForProfile/CardForProfile';
@@ -11,28 +12,35 @@ import { ApplaudT } from '@/types/ApplaudT';
 import { MemberT } from '@/types/MemberT';
 import Inbox from '@/components/Inbox/Inbox';
 
-const Profile = () => {
-  const [member, setMember] = useState<MemberT>();
-  const [individualApplauds, setIndividualApplauds] = useState<ApplaudT[]>([]);
-  const { data: session } = useSession();
+const Profile = async () => {
+  // const [member, setMember] = useState<MemberT>();
+  // const [individualApplauds, setIndividualApplauds] = useState<ApplaudT[]>([]);
+  // const { data: session } = useSession();
+
+  const session = await getServerSession();
+  const memberEmail = session?.user?.email as string;
+
+  const applauds: ApplaudT[] = await getPublishedApplauds(memberEmail);
+  const members: MemberT[] = await getAllMembers();
+  const member = members.find((member) => member.email === memberEmail);
 
   const name = session?.user?.name as string;
   const firstName = session?.user?.name?.split(' ')[0] as string;
   const imageURL = session?.user?.image as string;
 
-  useEffect(() => {
-    if (!session) {
-      return;
-    }
-    const memberEmail = session.user?.email;
+  // useEffect(() => {
+  //   if (!session) {
+  //     return;
+  //   }
+  //   const memberEmail = session.user?.email;
 
-    (async () => {
-      const applauds = await getPublishedApplauds(memberEmail as string);
-      const members: MemberT[] = await getAllMembers();
-      setMember(members.find((member) => member.email === memberEmail));
-      setIndividualApplauds(applauds);
-    })();
-  }, [session]);
+  //   (async () => {
+  //     const applauds = await getPublishedApplauds(memberEmail as string);
+  //     const members: MemberT[] = await getAllMembers();
+  //     setMember(members.find((member) => member.email === memberEmail));
+  //     setIndividualApplauds(applauds);
+  //   })();
+  // }, [session]);
 
   return (
     <div className='flex flex-col mt-4 gap-10'>
@@ -40,9 +48,7 @@ const Profile = () => {
       <main className='flex flex-col items-center gap-10 mx-10'>
         <section className='flex flex-col gap-8 items-center w-full'>
           <div className='flex w-full flex-col gap-3'>
-            <div className='flex justify-end'>
-              {session && <Inbox session={session} />}
-            </div>
+            <div className='flex justify-end'>{session && <Inbox />}</div>
             <div className='flex items-center justify-center w-full gap-8 px-2 py-3'>
               {session && (
                 <Image
@@ -64,7 +70,7 @@ const Profile = () => {
         </section>
         <h3 className='sub-title'>{firstName}&apos;s applauds</h3>
         <section className='flex flex-col w-full'>
-          <CardForProfile applauds={individualApplauds} />
+          <CardForProfile applauds={applauds} />
           <MockAppluadCards
             firstName={firstName}
             imageURL={imageURL}
