84f60a6aedce1d9e5a28155c2b5701b8d423ae60
lvan-ni <g.nilvan@icloud.com>
2023-11-16 18:45:58 +0100

    Add BackButton component with image and navigation functionality

---
+++ src/components/BackButton/BackButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import back from '@/assets/nav/back.png';
+
+const BackButton = () => {
+  const router = useRouter();
+
+  const goBack = () => {
+    router.back();
+  }
+  return (
+    <button onClick={goBack} className='cursor-pointer'>
+       <Image
+          src={back}
+          alt='back'
+          width={30}
+          height={30}
+        ></Image>
+    </button>
+  )
+}
+
+export default BackButton
