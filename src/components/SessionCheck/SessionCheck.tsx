8ca9a44eec54e61c5711b8c1306d896a2887fa53
lvan-ni <g.nilvan@icloud.com>
2023-11-16 18:45:48 +0100

    Refactor SessionCheck component to improve session handling

--- src/components/SessionCheck/SessionCheck.tsx
+++ src/components/SessionCheck/SessionCheck.tsx
@@ -1,31 +1,21 @@
 'use client';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
 
 const SessionCheck = () => {
-  const { data: session, status } = useSession();
+  const {data: session, status} = useSession();
   const router = useRouter();
   const pathname = usePathname();
 
   useEffect(() => {
-    if (status === 'loading') {
-      return;
-    }
-    if (!session) {
+    if (status !== 'loading' && !session && !['/', '/login', '/about'].includes(pathname)) {
       router.push('/landing');
     }
-  }, [session, status, router]);
+  }, [status, session, pathname, router]);
 
-  return (
-    <>
-      {pathname === '/about' ? null : (
-        <div className='bg-light w-full h-screen'></div>
-      )}
-      ;
-    </>
-  );
+  return null;
 };
 
 export default SessionCheck;
