8d919dcfaafcefabc47c856198fba4ce45a5eca5
ica1130 <ilijakrilovic@gmail.com>
2023-11-29 13:47:01 +0100

    refactor: Member page:
    Now view Member page is showing fetched data, not mocked.

--- src/app/member/[memberId]/page.tsx
+++ src/app/member/[memberId]/page.tsx
@@ -1,13 +1,13 @@
 // 'use client';
 // import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import { getAllMembers, getPublishedApplauds } from '@/libs/DB';
-import Header from '@/components/Header/Header';
-import ProfileInfo from '@/components/ProfileInfo/ProfileInfo';
-import CardForProfile from '@/components/CardForProfile/CardForProfile';
-import MockAppluadCards from '@/components/MockAppluadCards/MockAppluadCards';
+import Image from "next/image";
+import { getAllMembers, getPublishedApplauds } from "@/libs/DB";
+import Header from "@/components/Header/Header";
+import ProfileInfo from "@/components/ProfileInfo/ProfileInfo";
+import CardForProfile from "@/components/CardForProfile/CardForProfile";
+import MockAppluadCards from "@/components/MockAppluadCards/MockAppluadCards";
 // import { ApplaudT } from '@/types/ApplaudT';
-import { MemberT } from '@/types/MemberT';
+import { MemberT } from "@/types/MemberT";
 
 const MemberProfile = async ({ params }: { params: { memberId: string } }) => {
   // const [member, setMember] = useState<MemberT>();
@@ -21,8 +21,11 @@ const MemberProfile = async ({ params }: { params: { memberId: string } }) => {
     member?.email as string
   );
 
-  const firstName = member?.name.split(' ')[0] as string;
+  const firstName = member?.name.split(" ")[0] as string;
   const fullName = member?.name as string;
+  const bio = member?.bio as string;
+  const skills = member?.skills?.split(",") as string[];
+  const experience = member?.experience as string;
 
   // useEffect(() => {
   //   (async () => {
@@ -41,28 +44,28 @@ const MemberProfile = async ({ params }: { params: { memberId: string } }) => {
   // }, [member]);
 
   return (
-    <div className='flex flex-col mt-4 gap-10'>
+    <div className="flex flex-col mt-4 gap-10">
       <Header />
-      <main className='flex flex-col items-center mx-10 gap-10'>
-        <section className='flex flex-col gap-8 items-center w-full'>
-          <div className='flex items-center justify-center w-full gap-8 px-2 py-3'>
+      <main className="flex flex-col items-center mx-10 gap-10">
+        <section className="flex flex-col gap-8 items-center w-full">
+          <div className="flex items-center justify-center w-full gap-8 px-2 py-3">
             <Image
               src={member?.avatarUrl!}
-              alt='Profile photo'
+              alt="Profile photo"
               width={88}
               height={88}
-              className='rounded-full border border-silver'
+              className="rounded-full border border-silver"
             ></Image>
-            <div className='w-3/5'>
-              <h4 className='body-large'>{fullName}</h4>
-              <p className='body-small'>{member?.jobTitle}</p>
-              <p className='body-small'>{member?.company}</p>
+            <div className="w-3/5">
+              <h4 className="body-large">{fullName}</h4>
+              <p className="body-small">{member?.jobTitle}</p>
+              <p className="body-small">{member?.company}</p>
             </div>
           </div>
-          <ProfileInfo name={fullName} />
+          <ProfileInfo bio={bio} skills={skills} experience={experience} />
         </section>
-        <h3 className='sub-title'>{firstName}&apos;s applauds</h3>
-        <section className='flex flex-col w-full'>
+        <h3 className="sub-title">{firstName}&apos;s applauds</h3>
+        <section className="flex flex-col w-full">
           <CardForProfile applauds={individualApplauds} />
           <MockAppluadCards
             firstName={firstName}
