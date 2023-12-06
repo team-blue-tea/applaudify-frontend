5d24a90e2e86e8931c0fa6fbcaca455e454fe3c3
Palmn72 <cs.palmqvist@gmail.com>
2023-12-06 16:19:53 +0100

    conditional header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { useSession, signOut } from 'next-auth/react';
 import { motion, AnimatePresence } from 'framer-motion';
 import closeMenu from '@/assets/nav/close-menu.png';
+import { useWindowSize } from '@uidotdev/usehooks';
 
 const Header = () => {
   const { data: session } = useSession();
@@ -12,6 +13,8 @@ const Header = () => {
   const toggleMenu = () => {
     setDropDown(!dropDown);
   };
+
+  const windowSize = useWindowSize();
 
   const navLinks = [
     {
@@ -31,24 +34,41 @@ const Header = () => {
     },
   ];
 
+  const navLinksDesktop = [
+    {
+      text: 'profile',
+      href: '/profile',
+    },
+    {
+      text: 'about',
+      href: '/about',
+    },
+    {
+      text: 'docs',
+      href: 'https://github.com/orgs/team-blue-tea/repositories',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    },
+  ];
+
   const menuVariants = {
     initial: {
       opacity: 0,
-      scale: 0.95
+      scale: 0.95,
     },
     animate: {
       opacity: 1,
       scale: 1,
       transition: {
         duration: 0.2,
-        ease: "easeInOut"
+        ease: 'easeInOut',
       },
     },
     exit: {
       opacity: 0,
       transition: {
         duration: 0.2,
-        ease: "easeInOut"
+        ease: 'easeInOut',
       },
     },
   };
@@ -86,97 +106,126 @@ const Header = () => {
     },
   };
 
-  return (
-    <header className='sticky top-0 px-10 py-2.5 backdrop-blur-xl border-b bg-light/50 border-silver/50 flex flex-col w-full justify-between items-center gap-3 z-10'>
-      <div className='flex w-full bg-transparent items-center justify-between'>
-        <Link href='/'>
-          <h1 className='header ombre-text'>applaudify</h1>
-        </Link>
-        <div className='flex bg-transparent items-center gap-3'>
-          <div
-            className='header-nav'
-            onClick={toggleMenu}
-          >
-            Menu
-          </div>
-          <AnimatePresence>
-            {dropDown && (
-              <motion.div
-                variants={menuVariants}
-                initial='initial'
-                animate='animate'
-                exit='exit'
-                className='origin-top h-screen w-full fixed left-0 top-0 gap-40 px-10 py-4 z-10 bg-light'
-              >
-                <div className='flex h-full flex-col'>
-                  <div className='flex justify-between items-center'>
-                    <Link href='/'>
-                      <h1 className='header ombre-text'>applaudify</h1>
-                    </Link>
-                    <Image
-                      src={closeMenu}
-                      alt='close menu'
-                      width={30}
-                      height={30}
-                      onClick={toggleMenu}
-                    ></Image>
-                  </div>
-                  <motion.div
-                    variants={menuItemsContainerVariants}
-                    initial='initial'
-                    animate='open'
-                    exit='initial'
-                    className='flex flex-col h-full justify-center items-center gap-10'
+  if (windowSize.width && windowSize.width > 660) {
+    return (
+      <header className="sticky top-0 px-10 py-2.5 backdrop-blur-xl border-b bg-light/50 border-silver/50 flex flex-col w-full justify-between items-center gap-3 z-10">
+        <div className="flex w-full bg-transparent items-center justify-between">
+          <Link href="/" className="self-start">
+            <h1 className="header ombre-text">applaudify</h1>
+          </Link>
+          <div className="flex bg-transparent items-center gap-3 ml-10 w-full">
+            {navLinksDesktop.map((link, index) => {
+              return (
+                <div className="overflow-hidden" key={index + link.text}>
+                  <Link
+                    href={link.href}
+                    rel={link.rel}
+                    target={link.target}
+                    className="sub-title  text-2xl"
                   >
-                    {navLinks.map((link, index) => {
-                      return (
-                        <div
-                          className='overflow-hidden'
-                          key={index + link.text}
-                        >
-                          <motion.div
-                            variants={menuItemVariants}
-                            initial='initial'
-                            animate='open'
-                            exit='initial'
-                          >
-                            <Link
-                              href={link.href}
-                              rel={link.rel}
-                              target={link.target}
-                              onClick={link.onClick}
-                              className='sub-title'
-                            >
-                              {link.text}
-                            </Link>
-                          </motion.div>
-                        </div>
-                      );
-                    })}
-                  </motion.div>
+                    {link.text}
+                  </Link>
                 </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
-          {session ? (
+              );
+            })}
             <Link
-              href='/profile'
-              className='header-nav'
+              className="ml-auto"
+              href={session ? '' : '/login'}
+              onClick={
+                session ? () => signOut({ callbackUrl: '/' }) : undefined
+              }
             >
-              Profile
+              {session ? 'Sign out' : 'Login / Signup'}
             </Link>
-          ) : (
-            <Link
-              href='/login'
-              className='header-nav'
-            >
-              Login
-            </Link>
-          )}
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    return (
+      <header className="sticky top-0 px-10 py-2.5 backdrop-blur-xl border-b bg-light/50 border-silver/50 flex flex-col w-full justify-between items-center gap-3 z-10">
+        <div className="flex w-full bg-transparent items-center justify-between">
+          <Link href="/">
+            <h1 className="header ombre-text">applaudify</h1>
+          </Link>
+          <div className="flex bg-transparent items-center gap-3">
+            <div className="header-nav" onClick={toggleMenu}>
+              Menu
+            </div>
+            <AnimatePresence>
+              {dropDown && (
+                <motion.div
+                  variants={menuVariants}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  className="origin-top h-screen w-full fixed left-0 top-0 gap-40 px-10 py-4 z-10 bg-light"
+                >
+                  <div className="flex h-full flex-col">
+                    <div className="flex justify-between items-center">
+                      <Link href="/">
+                        <h1 className="header ombre-text">applaudify</h1>
+                      </Link>
+                      <Image
+                        src={closeMenu}
+                        alt="close menu"
+                        width={30}
+                        height={30}
+                        onClick={toggleMenu}
+                        className="cursor-pointer"
+                      ></Image>
+                    </div>
+                    <motion.div
+                      variants={menuItemsContainerVariants}
+                      initial="initial"
+                      animate="open"
+                      exit="initial"
+                      className="flex flex-col h-full justify-center items-center gap-10"
+                    >
+                      {navLinks.map((link, index) => {
+                        return (
+                          <div
+                            className="overflow-hidden"
+                            key={index + link.text}
+                          >
+                            <motion.div
+                              variants={menuItemVariants}
+                              initial="initial"
+                              animate="open"
+                              exit="initial"
+                            >
+                              <Link
+                                href={link.href}
+                                rel={link.rel}
+                                target={link.target}
+                                onClick={link.onClick}
+                                className="sub-title"
+                              >
+                                {link.text}
+                              </Link>
+                            </motion.div>
+                          </div>
+                        );
+                      })}
+                    </motion.div>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            {session ? (
+              <Link href="/profile" className="header-nav">
+                Profile
+              </Link>
+            ) : (
+              <Link href="/login" className="header-nav">
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </header>
+    );
+  }
 };
 
 export default Header;
