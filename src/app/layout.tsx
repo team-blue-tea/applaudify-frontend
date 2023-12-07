7c21eb0adbcb586ed6852231dee1b6ce3c5753d9
Palmn72 <cs.palmqvist@gmail.com>
2023-12-07 10:21:19 +0100

    feat: added desktopheader to layout, conditional render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import NextAuthProviders from '@/utils/nextAuthProviders';
 import { Josefin_Sans } from 'next/font/google';
 import './globals.css';
 import SessionCheck from '@/components/SessionCheck/SessionCheck';
+import DesktopHeader from '@/components/Header/DesktopHeader';
 
 const josefin = Josefin_Sans({ subsets: ['latin'] });
 
@@ -17,11 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={josefin.className}>
         <NextAuthProviders>
           <SessionCheck />
+          <DesktopHeader />
           {children}
+          <p className="small text-center text-stone mt-20">
+            © 2023 Applaudify
+          </p>
         </NextAuthProviders>
       </body>
     </html>
