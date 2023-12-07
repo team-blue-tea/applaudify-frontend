c4beb046b8755c97228ad5a7821127feac90ee9a
Palmn72 <cs.palmqvist@gmail.com>
2023-12-07 14:29:23 +0100

    refactor import

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,25 +1,20 @@
 'use client';
-import React from 'react'
+import React from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import back from '@/assets/nav/back.png';
+import { back } from '@/assets';
 
 const BackButton = () => {
   const router = useRouter();
 
   const goBack = () => {
     router.back();
-  }
+  };
   return (
-    <button onClick={goBack} className='cursor-pointer'>
-       <Image
-          src={back}
-          alt='back'
-          width={30}
-          height={30}
-        ></Image>
+    <button onClick={goBack} className="cursor-pointer">
+      <Image src={back} alt="back" width={30} height={30}></Image>
     </button>
-  )
-}
+  );
+};
 
-export default BackButton
+export default BackButton;
