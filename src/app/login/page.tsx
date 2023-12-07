cb44103aa3eb76ce534d62b2ca2b1910b55cfff8
Palmn72 <cs.palmqvist@gmail.com>
2023-12-07 13:59:53 +0100

    refactor import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,44 +4,34 @@ import { signIn } from 'next-auth/react';
 import Image from 'next/image';
 import github from '@/assets/login/github.png';
 import linkedin from '@/assets/login/linkedin.png';
-import BackButton from '@/components/BackButton/BackButton';
+import { BackButton } from '@/components';
 
 const Login = () => {
   return (
-    <div className='flex flex-col mx-10 my-14 gap-10'>
-      <header className='flex justify-start'>
+    <div className="flex flex-col mx-10 my-14 gap-10">
+      <header className="flex justify-start">
         <BackButton />
       </header>
-      <div className='flex flex-col items-center gap-36'>
-        <div className='flex flex-col gap-5'>
-          <h1 className='title-logo ombre-text'>applaudify</h1>
-          <h3 className='small-header text-center text-charcoal'>
+      <div className="flex flex-col items-center gap-36">
+        <div className="flex flex-col gap-5">
+          <h1 className="title-logo ombre-text">applaudify</h1>
+          <h3 className="small-header text-center text-charcoal">
             login / signup
           </h3>
         </div>
-        <div className='flex flex-col items-center gap-6 w-4/5 '>
+        <div className="flex flex-col items-center gap-6 w-4/5 ">
           <button
-            className='button login-btn'
+            className="button login-btn"
             onClick={() => signIn('github', { callbackUrl: '/home' })}
           >
-            <Image
-              src={github}
-              alt='github'
-              width={20}
-              height={20}
-            ></Image>
+            <Image src={github} alt="github" width={20} height={20}></Image>
             Continue with GitHub
           </button>
           <button
-            className='button login-btn'
+            className="button login-btn"
             onClick={() => signIn('linkedin', { callbackUrl: '/home' })}
           >
-            <Image
-              src={linkedin}
-              alt='linkedin'
-              width={20}
-              height={20}
-            ></Image>
+            <Image src={linkedin} alt="linkedin" width={20} height={20}></Image>
             Continue with LinkedIn
           </button>
         </div>
