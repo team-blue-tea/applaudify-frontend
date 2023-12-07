4d86277b1b5a9366fd5a8c96c201662bbfe50500
Palmn72 <cs.palmqvist@gmail.com>
2023-12-07 13:59:48 +0100

    refactor import

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { signOut, useSession } from 'next-auth/react';
 // import { getServerSession } from 'next-auth';
 import Link from 'next/link';
-import Header from '@/components/Header/Header';
+import { Header } from '@/components';
 
 const Landing = async () => {
   return (
