9265b2ca30a8cc9522644554b84b6fc8654ea219
Palmn72 <cs.palmqvist@gmail.com>
2023-12-07 14:00:46 +0100

    index file, export deault

---
+++ src/assets/about/index.ts
@@ -0,0 +1,5 @@
+export { default as Ilija } from './ilija.png';
+export { default as Lvan } from './lvan.png';
+export { default as Tim } from './tim.png';
+export { default as Sudha } from './sudha.png';
+export { default as Sebastian } from './seb.png';
