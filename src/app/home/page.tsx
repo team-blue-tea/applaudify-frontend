089cf09613b52dcb8d482c1fd3ccc1f0d0d87d81
Palmn72 <cs.palmqvist@gmail.com>
2023-12-07 11:58:57 +0100

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,28 +1,15 @@
-"use client";
-import { getAllApplauds } from "@/libs/DB";
-import CardForHome from "@/components/CardForHome/CardForHome";
-import Header from "@/components/Header/Header";
-import NewMemberCheck from "@/components/NewMemberCheck/NewMemeberCheck";
-import { useEffect, useState } from "react";
-import { ApplaudT } from "@/types/ApplaudT";
+import CardForHome from '@/components/CardForHome/CardForHome';
+import Header from '@/components/Header/Header';
+import NewMemberCheck from '@/components/NewMemberCheck/NewMemberCheck';
 
 const Home = () => {
-  const [applauds, setApplauds] = useState<ApplaudT[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const applauds = await getAllApplauds();
-      setApplauds(applauds);
-    })();
-  }, []);
-
   return (
     <div className="flex flex-col gap-10 mt-4">
       <Header />
       <NewMemberCheck />
       <main className="flex flex-col gap-8 mt-1 mx-10">
         <h1 className="small-header">👏 Spotlights</h1>
-        <CardForHome applauds={applauds} />
+        <CardForHome />
       </main>
     </div>
   );
--- src/components/CardForHome/CardForHome.tsx
+++ src/components/CardForHome/CardForHome.tsx
@@ -1,13 +1,21 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
-import { ApplaudT } from '@/types/ApplaudT';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { getAllApplauds } from '@/libs/DB';
+import { ApplaudT } from '@/types/ApplaudT';
 
-type CardForHomeProps = {
-  applauds: ApplaudT[];
-};
+const CardForHome = () => {
+  const [applauds, setApplauds] = useState<ApplaudT[]>([]);
 
-const CardForHome: React.FC<CardForHomeProps> = ({ applauds }) => {
+  useEffect(() => {
+    (async () => {
+      const applauds = await getAllApplauds();
+      setApplauds(applauds);
+    })();
+  }, []);
+
   return (
     <div>
       {[...applauds]
@@ -21,51 +29,48 @@ const CardForHome: React.FC<CardForHomeProps> = ({ applauds }) => {
             day: 'numeric',
           });
           return (
-            <section
-              key={applauds.indexOf(applaud)}
-              className='applaud-card'
-            >
+            <section key={applauds.indexOf(applaud)} className="applaud-card">
               <Link href={`/member/${receiver.id}`}>
-                <article className='receiver-layout'>
+                <article className="receiver-layout">
                   <Image
                     src={receiver.avatarUrl}
-                    alt='Receiver Profile'
+                    alt="Receiver Profile"
                     width={60}
                     height={60}
-                    className='profile-img'
+                    className="profile-img"
                   ></Image>
                   <div>
-                    <h4 className='receiver-name text-center'>
+                    <h4 className="receiver-name text-center">
                       {receiver.name}
                     </h4>
-                    <p className='receiver-info text-center'>
+                    <p className="receiver-info text-center">
                       {receiver.jobTitle}
                     </p>
-                    <p className='receiver-info text-center'>
+                    <p className="receiver-info text-center">
                       {receiver.company}
                     </p>
                   </div>
                 </article>
               </Link>
-              <p className='send-date'>{dateString}</p>
-              <p className='p-2.5 body-main text-center'>
+              <p className="send-date">{dateString}</p>
+              <p className="p-2.5 body-main text-center">
                 &apos;{comment}&apos;
               </p>
-              <article className='flex flex-col items-center'>
-                <p className='sender-info text-stone'>From</p>
+              <article className="flex flex-col items-center">
+                <p className="sender-info text-stone">From</p>
                 <Link href={`/member/${sender.id}`}>
-                  <div className='sender-layout'>
+                  <div className="sender-layout">
                     <Image
                       src={sender.avatarUrl}
-                      alt='Sender Profile'
+                      alt="Sender Profile"
                       width={30}
                       height={30}
-                      className='profile-img'
+                      className="profile-img"
                     ></Image>
-                    <div className='flex w-full justify-between items-end'>
+                    <div className="flex w-full justify-between items-end">
                       <div>
-                        <h4 className='sender'>{sender.name}</h4>
-                        <p className='sender-info'>{sender.jobTitle}</p>
+                        <h4 className="sender">{sender.name}</h4>
+                        <p className="sender-info">{sender.jobTitle}</p>
                       </div>
                     </div>
                   </div>
