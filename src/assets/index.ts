6a000c032047eef2e648055681635acbe138542c
Palmn72 <cs.palmqvist@gmail.com>
2023-12-13 09:15:32 +0100

    fix:  added "profile" to exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,15 +3,20 @@ export { default as Lvan } from './about/lvan.png';
 export { default as Tim } from './about/tim.png';
 export { default as Sudha } from './about/sudha.png';
 export { default as Sebastian } from './about/seb.png';
+
 export { default as back } from './nav/back.png';
 export { default as closeMenu } from './nav/close-menu.png';
 export { default as inboxNew } from './nav/inbox-new.png';
 export { default as inbox } from './nav/inbox.png';
 export { default as menu } from './nav/menu.png';
+export { default as profile } from './nav/profile.png';
+
 export { default as logo } from './logo.png';
 export { default as ombreLogoSmall } from './ombre-logo-small.png';
 export { default as ombreLogo } from './ombre-logo.png';
 export { default as title } from './title.png';
+
 export { default as gitHub } from './login/github.png';
 export { default as linkedIn } from './login/linkedin.png';
+
 export { default as arrow } from './card/arrow.png';
